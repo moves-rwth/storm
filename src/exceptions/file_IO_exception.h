40de04a1fe15f2d55a3296a90bfd4abd81979c39
Thomas Heinemann <thomas.heinemann@rwth-aachen.de>
2012-08-19 21:16:29 +0200

    Added missing files (exceptions)

---
+++ src/exceptions/file_IO_exception.h
@@ -0,0 +1,33 @@
+/*
+ * file_IO_exception.h
+ *
+ *  Created on: 16.08.2012
+ *      Author: thomas
+ */
+
+#ifndef MRMC_EXCEPTIONS_FILE_IO_EXCEPTION_H_
+#define MRMC_EXCEPTIONS_FILE_IO_EXCEPTION_H_
+
+namespace mrmc {
+
+namespace exceptions {
+
+class file_IO_exception : public std::exception {
+   public:
+#ifdef _WIN32
+      file_IO_exception() : exception("::mrmc::file_IO_exception"){};
+      file_IO_exception(const char * const s): exception(s) {};
+#else
+      file_IO_exception() {};
+      file_IO_exception(const char * const s): exception() {};
+#endif
+      virtual const char* what() const throw(){
+         {  return "mrmc::file_IO_exception";  }
+      }
+};
+
+}
+
+}
+
+#endif /* MRMC_EXCEPTIONS_FILE_IO_EXCEPTION_H_ */
