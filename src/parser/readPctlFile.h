2eb08a603a6b7124b4d169e1f0e50ac3bcca3511
gereon <gereon.kremer@rwth-aachen.de>
2012-11-28 19:48:41 +0100

    made stuff compile with new CMakeLists started a parser built with boost::spirit. It compiles, but does not do anything useful yet...

---
+++ src/parser/readPctlFile.h
@@ -0,0 +1,15 @@
+#ifndef READPCTLFILE_H_
+#define READPCTLFILE_H_
+
+namespace mrmc {
+namespace parser {
+
+/*!
+ *	@brief Load PCTL file
+ */
+void readPctlFile(const char * filename);
+
+} // namespace parser
+} // namespace mrmc
+
+#endif /* READPCTLFILE_H_ */
