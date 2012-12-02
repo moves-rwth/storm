18b72bc8d7be0333f2227d3a652e04534664f6e2
dehnert <dehnert@cs.rwth-aachen.de>
2012-12-02 12:13:32 +0100

    Added necessary include of unistd.h (for close()) to parser. Removed flag MAP_DENYWRITE of mmap for Mac OS and Linux as it is non-existent and ignored, respectively. Changed call to stat64 to call to stat for MAC OS, as stat64 is deprecated and 64-bit mode is turned on by macro that is no correctly set during OS-Detection.

--- src/utility/osDetection.h
+++ src/utility/osDetection.h
@@ -2,8 +2,9 @@
 
 #if defined __linux__ || defined __linux
 	#define LINUX
-#elif defined TARGET_OS_MAC || defined __apple__
+#elif defined TARGET_OS_MAC || defined __apple__ || defined __APPLE__
 	#define MACOSX
+	#define _DARWIN_USE_64_BIT_INODE
 #elif defined _WIN32 || defined _WIN64
 	#define WINDOWS
 #else
