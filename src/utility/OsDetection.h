470fa4c4b73469fb058b6a451a3f3ba46e22a283
gereon <gereon.kremer@rwth-aachen.de>
2012-12-17 15:37:03 +0100

    added sys/mman.h for linux target

--- src/utility/OsDetection.h
+++ src/utility/OsDetection.h
@@ -3,6 +3,7 @@
 
 #if defined __linux__ || defined __linux
 #	define LINUX
+#	include <sys/mman.h>
 #elif defined TARGET_OS_MAC || defined __apple__ || defined __APPLE__
 #	define MACOSX
 #	define _DARWIN_USE_64_BIT_INODE
