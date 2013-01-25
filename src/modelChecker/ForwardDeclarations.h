70181387a38b66849624695b63255cf50a3a60f4
Lanchid <thomas.heinemann@rwth-aachen.de>
2013-01-25 17:37:56 +0100

    Add forward declarations

---
+++ src/modelChecker/ForwardDeclarations.h
@@ -0,0 +1,23 @@
+/*
+ * ForwardDeclarations.h
+ *
+ *  Created on: 14.01.2013
+ *      Author: thomas
+ */
+
+#ifndef STORM_MODELCHECKER_FORWARDDECLARATIONS_H_
+#define STORM_MODELCHECKER_FORWARDDECLARATIONS_H_
+
+
+namespace storm {
+
+namespace modelChecker {
+
+template <class Type>
+class DtmcPrctlModelChecker;
+
+} //namespace modelChecker
+
+} //namespace storm
+
+#endif /* STORM_MODELCHECKER_FORWARDDECLARATIONS_H_ */
