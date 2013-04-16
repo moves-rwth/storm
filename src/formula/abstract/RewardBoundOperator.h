7e4d09cb0170c3a6dc25e824f2d3abfb874a02c1
Lanchid <thomas.heinemann@rwth-aachen.de>
2013-04-16 14:32:23 +0200

    Added abstract reward operators.

---
+++ src/formula/abstract/RewardBoundOperator.h
@@ -0,0 +1,82 @@
+/*
+ * RewardBoundOperator.h
+ *
+ *  Created on: 19.10.2012
+ *      Author: Thomas Heinemann
+ */
+
+#ifndef STORM_FORMULA_ABSTRACT_REWARDBOUNDOPERATOR_H_
+#define STORM_FORMULA_ABSTRACT_REWARDBOUNDOPERATOR_H_
+
+#include "PathBoundOperator.h"
+#include "utility/ConstTemplates.h"
+
+namespace storm {
+namespace formula {
+namespace abstract {
+
+/*!
+ * @brief
+ * Class for a Abstract formula tree with a R (reward) operator node over a reward interval as root.
+ *
+ * Has a reward path formula as sub formula/tree.
+ *
+ * @par Semantics
+ * 	  The formula holds iff the reward of the reward path formula is inside the bounds
+ * 	  specified in this operator
+ *
+ * The subtree is seen as part of the object and deleted with it
+ * (this behavior can be prevented by setting them to NULL before deletion)
+ *
+ *
+ * @see AbstractStateFormula
+ * @see AbstractPathFormula
+ * @see ProbabilisticOperator
+ * @see ProbabilisticNoBoundsOperator
+ * @see AbstractFormula
+ */
+template<class T, class FormulaType>
+class RewardBoundOperator : public PathBoundOperator<T, FormulaType> {
+
+public:
+	/*!
+	 * Empty constructor
+	 */
+	RewardBoundOperator() : PathBoundOperator<T>(PathBoundOperator<T>::LESS_EQUAL, storm::utility::constGetZero<T>(), nullptr) {
+		// Intentionally left empty
+	}
+
+	/*!
+	 * Constructor
+	 *
+	 * @param comparisonRelation The relation to compare the actual value and the bound
+	 * @param bound The bound for the probability
+	 * @param pathFormula The child node
+	 */
+	RewardBoundOperator(
+			typename PathBoundOperator<T>::ComparisonType comparisonRelation, T bound, FormulaType* pathFormula) :
+				PathBoundOperator<T>(comparisonRelation, bound, pathFormula) {
+		// Intentionally left empty
+	}
+
+	RewardBoundOperator(
+			typename PathBoundOperator<T>::ComparisonType comparisonRelation, T bound, FormulaType* pathFormula, bool minimumOperator)
+			: PathBoundOperator<T>(comparisonRelation, bound, pathFormula, minimumOperator) {
+		// Intentionally left empty
+	}
+
+	/*!
+	 * @returns a string representation of the formula
+	 */
+	virtual std::string toString() const {
+		std::string result = "R ";
+		result += PathBoundOperator<T>::toString();
+		return result;
+	}
+};
+
+} //namespace abstract
+} //namespace formula
+} //namespace storm
+
+#endif /* STORM_FORMULA_ABSTRACT_REWARDBOUNDOPERATOR_H_ */
