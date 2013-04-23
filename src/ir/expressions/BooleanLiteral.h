766a92db3ab372a3c88f7827f4c3f0e6049b514d
gereon <gereon.kremer@rwth-aachen.de>
2013-04-23 20:28:31 +0200

    Fixed error in *Literal::clone()

--- src/ir/expressions/BooleanLiteral.h
+++ src/ir/expressions/BooleanLiteral.h
@@ -27,7 +27,7 @@ public:
 	}
 
 	virtual std::shared_ptr<BaseExpression> clone(const std::map<std::string, std::string>& renaming, const std::map<std::string, uint_fast64_t>& bools, const std::map<std::string, uint_fast64_t>& ints) {
-		return std::shared_ptr<BaseExpression>(this);
+		return std::shared_ptr<BaseExpression>(new BooleanLiteral(this->value));
 	}
 
 	virtual bool getValueAsBool(std::pair<std::vector<bool>, std::vector<int_fast64_t>> const* variableValues) const {
--- src/ir/expressions/IntegerLiteral.h
+++ src/ir/expressions/IntegerLiteral.h
@@ -24,11 +24,10 @@ public:
 	}
 
 	virtual ~IntegerLiteral() {
-
 	}
 
 	virtual std::shared_ptr<BaseExpression> clone(const std::map<std::string, std::string>& renaming, const std::map<std::string, uint_fast64_t>& bools, const std::map<std::string, uint_fast64_t>& ints) {
-		return std::shared_ptr<BaseExpression>(this);
+		return std::shared_ptr<BaseExpression>(new IntegerLiteral(this->value));
 	}
 
 	virtual int_fast64_t getValueAsInt(std::pair<std::vector<bool>, std::vector<int_fast64_t>> const* variableValues) const {
