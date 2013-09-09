df21fbbbdec9767fa730cded6d699686877978c6
PBerger <philipp.berger@rwth-aachen.de>
2013-09-09 05:03:51 +0200

    Added new version of Log4CPlus, updated from v1.1.0 to v.1.1.2-rc2 Updated CMakeLists.txt, included new Log4Cplus
    Former-commit-id: 55cc5a74d36c30627e005f90df994a11898b3dcb

--- resources/3rdparty/log4cplus-1.1.0/tests/filter_test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chris/Documents/workspace/mrmc/MRMC/resources/3rdparty/log4cplus-1.1.0/tests/filter_test
-# Build directory: /Users/chris/Documents/workspace/mrmc/MRMC/resources/3rdparty/log4cplus-1.1.0/tests/filter_test
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(filter_test "filter_test")
--- resources/3rdparty/log4cplus-1.1.0/tests/priority_test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chris/Documents/workspace/mrmc/MRMC/resources/3rdparty/log4cplus-1.1.0/tests/priority_test
-# Build directory: /Users/chris/Documents/workspace/mrmc/MRMC/resources/3rdparty/log4cplus-1.1.0/tests/priority_test
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(priority_test "priority_test")
