a49991484c11348a2a0843374f0c2836a2a9daf3
PBerger <philipp.berger@rwth-aachen.de>
2014-08-12 23:47:05 +0200

    Fixed missing definitions for the current working directory.
    Former-commit-id: cc991435262f888ece909d3811ce153147049636

--- src/utility/OsDetection.h
+++ src/utility/OsDetection.h
@@ -4,10 +4,12 @@
 #if defined __linux__ || defined __linux
 #	define LINUX
 #	include <sys/mman.h>
+#	include <unistd.h>
 #include <execinfo.h> // Required by ErrorHandling.h
 #include <cxxabi.h> // Required by ErrorHandling.h
 #include <sys/time.h> // Required by storm.cpp, Memory Usage
 #include <sys/resource.h> // Required by storm.cpp, Memory Usage
+#	define GetCurrentDir getcwd
 #elif defined TARGET_OS_MAC || defined __apple__ || defined __APPLE__
 #	define MACOSX
 #	define _DARWIN_USE_64_BIT_INODE
@@ -17,6 +19,7 @@
 #	include <cxxabi.h> // Required by ErrorHandling.h
 #	include <sys/time.h> // Required by storm.cpp, Memory Usage
 #	include <sys/resource.h> // Required by storm.cpp, Memory Usage
+#	define GetCurrentDir getcwd
 #elif defined _WIN32 || defined _WIN64
 #	define WINDOWS
 #	ifndef NOMINMAX
@@ -28,8 +31,10 @@
 #	include <winnt.h>
 #	include <DbgHelp.h>
 #	include <Psapi.h>
+#	include <direct.h>
 #	define strncpy strncpy_s
 #	define sscanf sscanf_s
+#	define GetCurrentDir _getcwd
 
 // This disables Warning C4250 - Diamond Inheritance Dominance
 #pragma warning(disable:4250)
