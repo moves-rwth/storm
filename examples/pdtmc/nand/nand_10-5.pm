b305a3b498e01672f2f7bfbcc80b4fd4c9082f7e
dehnert <dehnert@cs.rwth-aachen.de>
2014-12-03 14:19:46 +0100

    Switched to FactorizedPolynomial as the basis for rational functions and added missing reward construct for one NAND model.
    Former-commit-id: 8bb62ee1d20c86ea8580734b657e0ad8e8963128

--- examples/pdtmc/nand/nand_10-5.pm
+++ examples/pdtmc/nand/nand_10-5.pm
@@ -69,3 +69,8 @@ endmodule
 label "target" = s=4 & z/N<0.1;
 label "end" = s=4;
 
+// rewards: final value of gate
+rewards
+    // [] s=0 & (c=N) & (u=M) : z/N;
+    s=0 & (c=N) & (u=M) : z/N;
+endrewards
