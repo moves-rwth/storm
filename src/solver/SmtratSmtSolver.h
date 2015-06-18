a412ac2a6033454cad1e1f83af51065f0689cc64
sjunges <sebastian.junges@rwth-aachen.de>
2015-06-18 22:14:52 +0200

    Progress in smtrat-smtsolver interface
    Former-commit-id: 2cee8a49680ab9c4db8c31f5885b5cad6b4ea206

---
+++ src/solver/SmtratSmtSolver.h
@@ -0,0 +1,50 @@
+#ifndef STORM_SOLVER_SMTRATSMTSOLVER
+#define STORM_SOLVER_SMTRATSMTSOLVER
+#include "storm-config.h"
+#include "src/solver/SmtSolver.h"
+
+#ifdef STORM_HAVE_SMTRAT
+#include "lib/smtrat.h"
+#include "../adapters/carlAdapter.h"
+
+
+namespace storm {
+	namespace solver {
+		class SmtratSmtSolver : public SmtSolver {
+                private:
+                        smtrat::RatOne* solver;
+                        unsigned exitCode;
+                    
+		public:
+			SmtratSmtSolver(storm::expressions::ExpressionManager& manager);
+			virtual ~SmtratSmtSolver();
+
+			virtual void push() override;
+
+			virtual void pop() override;
+
+			virtual void pop(uint_fast64_t n) override;
+
+			virtual CheckResult check() override;
+#ifdef STORM_HAVE_CARL 
+                        void add(storm::RawPolynomial const&, storm::CompareRelation);
+                        
+                        template<typename ReturnType>
+                        ReturnType  getModel() const;
+                        
+                        std::vector<smtrat::FormulasT> const& getUnsatisfiableCores() const;
+                        
+                        
+                        
+                        
+                        
+#endif
+            
+            // The last result that was returned by any of the check methods.
+			CheckResult lastResult;
+		};
+	}
+}
+#endif
+
+#endif // STORM_SOLVER_SMTRATSMTSOLVER
