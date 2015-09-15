deec423f2795807d1033797bd07ada2f96737710
dehnert <dehnert@cs.rwth-aachen.de>
2015-09-15 14:21:34 +0200

    fixed infinite recursion in constants comparator
    Former-commit-id: b36b352d3690110f6795ac2ef7edfb7a24467bd4

--- src/utility/ConstantsComparator.cpp
+++ src/utility/ConstantsComparator.cpp
@@ -11,12 +11,12 @@ namespace storm {
     namespace utility {
         template<typename ValueType>
         bool ConstantsComparator<ValueType>::isOne(ValueType const& value) const {
-            return isOne(value);
+            return storm::utility::isOne(value);
         }
         
         template<typename ValueType>
         bool ConstantsComparator<ValueType>::isZero(ValueType const& value) const {
-            return isZero(value);
+            return storm::utility::isZero(value);
         }
         
         template<typename ValueType>
@@ -26,7 +26,7 @@ namespace storm {
         
         template<typename ValueType>
         bool ConstantsComparator<ValueType>::isConstant(ValueType const& value) const {
-            return isConstant(value);
+            return storm::utility::isConstant(value);
         }
 
         template<typename ValueType>
