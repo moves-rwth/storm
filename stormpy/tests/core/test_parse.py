0ed3714c906e5705a14a7fe4c5ddf531a2c01e99
Mavo <matthias.volk@rwth-aachen.de>
2016-04-29 16:34:59 +0200

    Added test cases
    Former-commit-id: c9ea2edfff5d7c1fe48dbf17da958f3eaa2dc363

---
+++ stormpy/tests/core/test_parse.py
@@ -0,0 +1,7 @@
+import stormpy
+
+class TestParse:
+    def test_parse_program(self):
+        # Fix!!!
+        #s = stormpy.parse_program("../../examples/dtmc/die/die.pm")
+        pass
---
+++ stormpy/tests/logic/test_formulas.py
@@ -0,0 +1,6 @@
+import stormpy
+from stormpy.logic import logic
+
+class TestFormulas:
+    def test_formula(self):
+        formula = logic.Formula
