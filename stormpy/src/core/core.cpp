395fabe8671923cbebc1c6fdffc22f0757034063
Mavo <matthias.volk@rwth-aachen.de>
2016-04-29 17:00:36 +0200

    Set_up
    Former-commit-id: 5cbfbb3e43059bbf1c3ea17b8e2b4eca59b5581c

--- stormpy/src/core/core.cpp
+++ stormpy/src/core/core.cpp
@@ -6,11 +6,21 @@
 
 // Thin wrapper for initializing
 void setupStormLib(std::string const& args) {
-//    storm::utility::setUp();
-//    storm::settings::SettingsManager::manager().setFromString(args);
+    storm::utility::setUp();
+    storm::settings::SettingsManager::manager().setFromString(args);
 }
 
 void define_core(py::module& m) {
     
-    m.def("set_up", &setupStormLib);
+    m.def("set_up", &setupStormLib, "Initialize Storm");
+
+    m.def("parse_formulae", storm::parseFormulasForProgram, "Parse formula for program");
+    m.def("parse_program", storm::parseProgram, "Parse program");
+
+    //m.def("build_model", buildModel, return_value_policy<return_by_value>());
+
+    //m.def("build_model_from_prism_program", storm::buildSymbolicModel<double>);
+    //m.def("build_parametric_model_from_prism_program", storm::buildSymbolicModel<storm::RationalFunction>);
+
+
 }
---
+++ stormpy/tests/core/test_core.py
@@ -0,0 +1,5 @@
+import stormpy
+
+class TestCore:
+    def test_init(self):
+        stormpy.set_up("")
