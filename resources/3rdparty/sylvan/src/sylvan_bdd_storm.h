818760e0ad8b6dcd44fb3b237f74d58e641262c4
dehnert <dehnert@cs.rwth-aachen.de>
2016-05-06 19:17:24 +0200

    removed sylvan version from resources
    Former-commit-id: a81c5792d59e36fe94f0c1490ba3c4154e562689

--- resources/3rdparty/sylvan/src/sylvan_bdd_storm.h
+++
@@ -1,3 +0,0 @@
-#define bdd_isnegated(dd) ((dd & sylvan_complement) ? 1 : 0)
-#define bdd_regular(dd) (dd & ~sylvan_complement)
-#define bdd_isterminal(dd) (dd == sylvan_false || dd == sylvan_true)
--- resources/3rdparty/sylvan/src/sylvan_obj_bdd_storm.hpp
+++
@@ -1,3 +0,0 @@
-    Mtbdd toDoubleMtbdd() const;
-    Mtbdd toInt64Mtbdd() const;
-    Mtbdd Ite(Mtbdd const& thenDd, Mtbdd const& elseDd) const;
