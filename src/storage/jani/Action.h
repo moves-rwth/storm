91f8ecf17c3799c5e45bf90f3ec0ed33d9627801
sjunges <sebastian.junges@rwth-aachen.de>
2016-05-23 14:46:01 +0200

    Small class for actions
    Former-commit-id: 082f5ddddce25597ad9d49d339f000007ca2115e

---
+++ src/storage/jani/Action.h
@@ -0,0 +1,25 @@
+#include <string>
+
+#pragma once
+namespace storm {
+    namespace jani {
+        class Action {
+        public:
+            /**
+             * Creates an action
+             * @param name name of the action
+             */
+            Action(std::string const& name);
+
+            /**
+             * Returns the name of the location.
+             */
+            std::string const& getName() const;
+
+        private:
+            std::string name;
+
+        };
+
+    }
+}
