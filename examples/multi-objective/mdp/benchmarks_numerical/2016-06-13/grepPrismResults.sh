eaa50eb47eb467d0275098744e68ab3e9e5e7dc5
TimQu <tim.quatmann@rwth-aachen.de>
2016-06-14 14:30:42 +0200

    updated prism benchmark table
    Former-commit-id: d05b06d64a8d0b4521282c7407552d74779258ea

--- examples/multi-objective/mdp/benchmarks_numerical/2016-06-13/grepPrismResults.sh
+++ examples/multi-objective/mdp/benchmarks_numerical/2016-06-13/grepPrismResults.sh
@@ -4,7 +4,7 @@ cat *.output | grep construc -A3 | grep States >> results.csv
 echo " " >> results.csv 
 cat *.output | grep "Number of weight vectors used:" >> results.csv 
 echo " " >> results.csv 
-cat *.output | grep "value iteration(s) took" >> results.csv
+cat *.output | grep "Multi-objective value iterations took" >> results.csv
 echo " " >> results.csv 
 cat *.output | grep "Time for model checking:" >> results.csv
 echo " " >> results.csv 
