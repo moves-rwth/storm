40a79485402c029d6e6c91f55afdd1053283e8d4
dehnert <dehnert@cs.rwth-aachen.de>
2016-06-22 23:12:13 +0200

    started generalizing elimination to equation system solving
    Former-commit-id: aabe89b65f6c929c45bb349c7354e5d4bab681b2

--- src/solver/stateelimination/StateEliminator.h
+++ src/solver/stateelimination/StateEliminator.h
@@ -1,35 +1,18 @@
 #ifndef STORM_SOLVER_STATEELIMINATION_STATEELIMINATOR_H_
 #define STORM_SOLVER_STATEELIMINATION_STATEELIMINATOR_H_
 
-#include "src/storage/sparse/StateType.h"
-
-#include "src/storage/FlexibleSparseMatrix.h"
-#include "src/storage/BitVector.h"
+#include "src/solver/stateelimination/EliminatorBase.h"
 
 namespace storm {
     namespace solver {
         namespace stateelimination {
             
             template<typename ValueType>
-            class StateEliminator {
+            class StateEliminator : public EliminatorBase<ValueType, ScalingMode::DivideOneMinus> {
             public:
-                typedef typename storm::storage::FlexibleSparseMatrix<ValueType>::row_type FlexibleRowType;
-                typedef typename FlexibleRowType::iterator FlexibleRowIterator;
-
                 StateEliminator(storm::storage::FlexibleSparseMatrix<ValueType>& transitionMatrix, storm::storage::FlexibleSparseMatrix<ValueType>& backwardTransitions);
                 
-                void eliminateState(storm::storage::sparse::state_type state, bool removeForwardTransitions, storm::storage::BitVector predecessorConstraint = storm::storage::BitVector());
-                
-                // Provide virtual methods that can be customized by subclasses to govern side-effect of the elimination.
-                virtual void updateValue(storm::storage::sparse::state_type const& state, ValueType const& loopProbability);
-                virtual void updatePredecessor(storm::storage::sparse::state_type const& predecessor, ValueType const& probability, storm::storage::sparse::state_type const& state);
-                virtual void updatePriority(storm::storage::sparse::state_type const& state);
-                virtual bool filterPredecessor(storm::storage::sparse::state_type const& state);
-                virtual bool isFilterPredecessor() const;
-                
-            protected:
-                storm::storage::FlexibleSparseMatrix<ValueType>& transitionMatrix;
-                storm::storage::FlexibleSparseMatrix<ValueType>& backwardTransitions;
+                void eliminateState(storm::storage::sparse::state_type state, bool removeForwardTransitions);
             };
             
         } // namespace stateelimination
