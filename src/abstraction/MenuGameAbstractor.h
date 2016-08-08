28d2471d4d43f660ffe50dbe41e0c7b4cf393114
dehnert <dehnert@cs.rwth-aachen.de>
2016-08-08 16:50:22 +0200

    started on menu game abstractor interface
    Former-commit-id: 73ca717aec07eb8f030e67c1f09e344aecded09e

--- src/abstraction/MenuGameAbstractor.h
+++ src/abstraction/MenuGameAbstractor.h
@@ -2,14 +2,16 @@
 
 #include "src/storage/dd/DdType.h"
 
+#include "src/abstraction/MenuGame.h"
+
 namespace storm {
     namespace abstraction {
         
         template <storm::dd::DdType DdType, typename ValueType>
         class MenuGameAbstractor {
-            
-            
-            
+        public:
+            virtual storm::abstraction::MenuGame<DdType, ValueType> abstract() = 0;
+            virtual void refine(std::vector<storm::expressions::Expression> const& predicates) = 0;
         };
         
     }
