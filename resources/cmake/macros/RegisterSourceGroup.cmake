2a1c317363020e6cb7dc6ec72851b0d240e0ebce
sjunges <sebastian.junges@rwth-aachen.de>
2016-08-09 16:42:25 +0200

    removed debug output from cmake
    Former-commit-id: f443a42ba92e240777d767d0a09f2660eecf24b5

--- resources/cmake/macros/RegisterSourceGroup.cmake
+++ resources/cmake/macros/RegisterSourceGroup.cmake
@@ -1,13 +1,11 @@
 macro(register_source_groups_from_filestructure list_of_files)
     foreach(FILE ${list_of_files})
-        message(${FILE})
         get_filename_component(PARENT_DIR "${FILE}" PATH)
 
         # skip src or include and changes /'s to \\'s
         string(REPLACE ${PROJECT_SOURCE_DIR} "" PARENT_DIR "${PARENT_DIR}")
         string(REGEX REPLACE "(\\./)?(src|include)/?" "" GROUP "${PARENT_DIR}")
         string(REPLACE "/" "\\" GROUP "${GROUP}")
-        message(${GROUP})
 
         # group into "Source Files" and "Header Files"
         # if ("${FILE}" MATCHES ".*\\.cpp")
