ae3394ff5c8a15c9cb7f2946e8ca6febdca2928b
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2016-11-18 17:46:58 +0100

    extended gspn export settigns

--- src/storm/settings/modules/GSPNExportSettings.cpp
+++ src/storm/settings/modules/GSPNExportSettings.cpp
@@ -16,11 +16,17 @@ namespace storm {
             const std::string GSPNExportSettings::moduleName = "exportGspn";
             
             const std::string GSPNExportSettings::writeToDotOptionName = "gspn-dot-output";
+            
+            const std::string GSPNExportSettings::writeToPnmlOptionName = "to-pnml";
+            const std::string GSPNExportSettings::writeToPnproOptionName = "to-pnpro";
+            
             //const std::string GSPNExportSettings::janiFileOptionShortName = "dotoutput";
             
             
             GSPNExportSettings::GSPNExportSettings() : ModuleSettings(moduleName) {
                 this->addOption(storm::settings::OptionBuilder(moduleName, writeToDotOptionName, false, "Destination for the dot output.").addArgument(storm::settings::ArgumentBuilder::createStringArgument("filename", "path to file").build()).build());
+                this->addOption(storm::settings::OptionBuilder(moduleName, writeToPnmlOptionName, false, "Destination for the pnml output").addArgument(storm::settings::ArgumentBuilder::createStringArgument("filename", "path to file").build()).build());
+                this->addOption(storm::settings::OptionBuilder(moduleName, writeToPnproOptionName, false, "Destination for the pnpro output").addArgument(storm::settings::ArgumentBuilder::createStringArgument("filename", "path to file").build()).build());
             }
             
             bool GSPNExportSettings::isWriteToDotSet() const {
@@ -30,6 +36,23 @@ namespace storm {
             std::string GSPNExportSettings::getWriteToDotFilename() const {
                 return this->getOption(writeToDotOptionName).getArgumentByName("filename").getValueAsString();
             }
+            
+            bool GSPNExportSettings::isWriteToPnmlSet() const {
+                return this->getOption(writeToPnmlOptionName).getHasOptionBeenSet();
+            }
+            
+            std::string GSPNExportSettings::getWriteToPnmlFilename() const {
+                return this->getOption(writeToPnmlOptionName).getArgumentByName("filename").getValueAsString();
+            }
+            
+            bool GSPNExportSettings::isWriteToPnproSet() const {
+                return this->getOption(writeToPnproOptionName).getHasOptionBeenSet();
+            }
+            
+            std::string GSPNExportSettings::getWriteToPnproFilename() const {
+                return this->getOption(writeToPnproOptionName).getArgumentByName("filename").getValueAsString();
+            }
+            
             
             void GSPNExportSettings::finalize() {
                 
--- src/storm/settings/modules/GSPNExportSettings.h
+++ src/storm/settings/modules/GSPNExportSettings.h
@@ -24,6 +24,20 @@ namespace storm {
                  */
                 std::string getWriteToDotFilename() const;
                 
+                bool isWriteToPnmlSet() const;
+                
+                /**
+                 *
+                 */
+                std::string getWriteToPnmlFilename() const;
+                
+                bool isWriteToPnproSet() const;
+                
+                /**
+                 *
+                 */
+                std::string getWriteToPnproFilename() const;
+                
                 
                 bool check() const override;
                 void finalize() override;
@@ -32,6 +46,8 @@ namespace storm {
                 
             private:
                 static const std::string writeToDotOptionName;
+                static const std::string writeToPnmlOptionName;
+                static const std::string writeToPnproOptionName;
                 //static const std::string writeToDotOptionShortName;
                 
             };
