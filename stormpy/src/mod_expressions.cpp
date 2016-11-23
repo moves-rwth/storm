c99ca64f863088c019b8f999f1864f2bc05ec7e8
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2016-11-23 15:49:28 +0100

    (Python) some simple methods on expressions

--- stormpy/src/mod_expressions.cpp
+++ stormpy/src/mod_expressions.cpp
@@ -9,6 +9,12 @@ PYBIND11_PLUGIN(expressions) {
         ;
     
     py::class_<storm::expressions::Expression>(m, "Expression", "Holds an expression")
+    .def("__str__", &storm::expressions::Expression::toString)
+    .def_property_readonly("contains_variables", &storm::expressions::Expression::containsVariables)
+    .def_property_readonly("has_boolean_type", &storm::expressions::Expression::hasBooleanType)
+    .def_property_readonly("has_integer_type", &storm::expressions::Expression::hasIntegerType)
+    .def_property_readonly("has_rational_type", &storm::expressions::Expression::hasRationalType)
+    
         ;
 
 	return m.ptr();
