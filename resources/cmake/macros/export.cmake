0260eaa7bebe51cef1700ed721b5a1a02ec41508
sjunges <sebastian.junges@gmail.com>
2016-11-27 22:54:46 +0100

    towards exporting targets for storm to be included in other projects

---
+++ resources/cmake/macros/export.cmake
@@ -0,0 +1,26 @@
+
+# Add all targets to the build-tree export set
+export(TARGETS ${STORM_TARGETS} FILE "${PROJECT_BINARY_DIR}/stormTargets.cmake")
+
+message(STATUS "Registered with cmake")
+# Export the package for use from the build-tree
+# (this registers the build-tree with a global CMake-registry)
+export(PACKAGE storm)
+
+set(DEP_TARGETS "")
+
+set(EXP_OPTIONS "")
+foreach(option ${EXPORTED_OPTIONS})
+    set(EXP_OPTIONS "${EXP_OPTIONS}\nset(${option} \"${${option}}\")")
+endforeach()
+
+include(CMakePackageConfigHelpers)
+
+set(CONF_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/src")
+message("CMAKE_INSTALL_DIR: ${CMAKE_INSTALL_DIR}")
+configure_package_config_file(
+        resources/cmake/stormConfig.cmake.in
+        ${PROJECT_BINARY_DIR}/stormConfig.cmake
+        INSTALL_DESTINATION ${CMAKE_INSTALL_DIR}
+        PATH_VARS INCLUDE_INSTALL_DIR #SYSCONFIG_INSTALL_DIR
+)
