e08fad7b4ab56887f569eef37ce4926498f22b10
TimQu <tim.quatmann@cs.rwth-aachen.de>
2017-01-04 14:30:43 +0100

    improved "Model checking property .." output a little.

--- src/storm/storage/jani/Property.cpp
+++ src/storm/storage/jani/Property.cpp
@@ -5,7 +5,7 @@ namespace storm {
         
         
         std::ostream& operator<<(std::ostream& os, FilterExpression const& fe) {
-            return os << "Obtain " << toString(fe.getFilterType()) << " the 'initial'-states with values described by '" << *fe.getFormula() << "'";
+            return os << "Obtain " << toString(fe.getFilterType()) << " of the 'initial'-states with values described by '" << *fe.getFormula() << "'";
         }
         
         Property::Property(std::string const& name, std::shared_ptr<storm::logic::Formula const> const& formula, std::string const& comment)
