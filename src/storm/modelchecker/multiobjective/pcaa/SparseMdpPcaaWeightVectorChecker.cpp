2e01c8b137ad0afe41722194797139eb6b0f43d5
TimQu <tim.quatmann@cs.rwth-aachen.de>
2017-04-12 16:45:08 +0200

    Fixed time bounds containing constant variables for multi objective formulas.

--- src/storm/modelchecker/multiobjective/pcaa/SparseMdpPcaaWeightVectorChecker.cpp
+++ src/storm/modelchecker/multiobjective/pcaa/SparseMdpPcaaWeightVectorChecker.cpp
@@ -5,6 +5,7 @@
 #include "storm/models/sparse/StandardRewardModel.h"
 #include "storm/utility/macros.h"
 #include "storm/utility/vector.h"
+#include "storm/exceptions/InvalidPropertyException.h"
 
 
 namespace storm {
@@ -19,7 +20,7 @@ namespace storm {
                                                                                                    storm::storage::BitVector const& possiblyRecurrentStates) :
                 SparsePcaaWeightVectorChecker<SparseMdpModelType>(model, objectives, actionsWithNegativeReward, ecActions, possiblyRecurrentStates) {
                 // set the state action rewards
-                for(uint_fast64_t objIndex = 0; objIndex < this->objectives.size(); ++objIndex) {
+                for (uint_fast64_t objIndex = 0; objIndex < this->objectives.size(); ++objIndex) {
                     typename SparseMdpModelType::RewardModelType const& rewModel = this->model.getRewardModel(this->objectives[objIndex].rewardModelName);
                     STORM_LOG_ASSERT(!rewModel.hasTransitionRewards(), "Reward model has transition rewards which is not expected.");
                     this->discreteActionRewards[objIndex] = rewModel.getTotalRewardVector(this->model.getTransitionMatrix());
@@ -34,19 +35,18 @@ namespace storm {
                 std::vector<ValueType> temporaryResult(this->model.getNumberOfStates());
                 std::vector<ValueType> zeroReward(weightedRewardVector.size(), storm::utility::zero<ValueType>());
                 // Get for each occurring timeBound the indices of the objectives with that bound.
-                std::map<uint_fast64_t, storm::storage::BitVector, std::greater<uint_fast64_t>> lowerTimeBounds;
-                std::map<uint_fast64_t, storm::storage::BitVector, std::greater<uint_fast64_t>> upperTimeBounds;
-                for(uint_fast64_t objIndex = 0; objIndex < this->objectives.size(); ++objIndex) {
+                std::map<uint_fast64_t, storm::storage::BitVector, std::greater<uint_fast64_t>> stepBounds;
+                for (uint_fast64_t objIndex = 0; objIndex < this->objectives.size(); ++objIndex) {
                     auto const& obj = this->objectives[objIndex];
-                    if(obj.lowerTimeBound) {
-                        auto timeBoundIt = lowerTimeBounds.insert(std::make_pair(storm::utility::convertNumber<uint_fast64_t>(*obj.lowerTimeBound), storm::storage::BitVector(this->objectives.size(), false))).first;
-                        STORM_LOG_WARN_COND(storm::utility::convertNumber<ValueType>(timeBoundIt->first) == (*obj.lowerTimeBound), "Rounded non-integral bound " << *obj.lowerTimeBound << " to " << timeBoundIt->first  << ".");
-                        timeBoundIt->second.set(objIndex);
-                    }
-                    if(obj.upperTimeBound) {
-                        auto timeBoundIt = upperTimeBounds.insert(std::make_pair(storm::utility::convertNumber<uint_fast64_t>(*obj.upperTimeBound), storm::storage::BitVector(this->objectives.size(), false))).first;
-                        STORM_LOG_WARN_COND(storm::utility::convertNumber<ValueType>(timeBoundIt->first) == (*obj.upperTimeBound), "Rounded non-integral bound " << *obj.upperTimeBound << " to " << timeBoundIt->first  << ".");
-                        timeBoundIt->second.set(objIndex);
+                    STORM_LOG_THROW(!obj.lowerTimeBound, storm::exceptions::InvalidPropertyException, "Lower step bounds are not supported by this model checker");
+                    if (obj.upperTimeBound) {
+                        STORM_LOG_THROW(!obj.upperTimeBound->containsVariables(), storm::exceptions::InvalidPropertyException, "The step bound '" << * obj.upperTimeBound << " contains undefined variables");
+                        uint_fast64_t stepBound = (uint_fast64_t) obj.upperTimeBound->evaluateAsInt();
+                        if (obj.upperTimeBoundStrict) {
+                            --stepBound;
+                        }
+                        auto stepBoundIt = stepBounds.insert(std::make_pair(stepBound, storm::storage::BitVector(this->objectives.size(), false))).first;
+                        stepBoundIt->second.set(objIndex);
                         
                         // There is no error for the values of these objectives.
                         this->offsetsToLowerBound[objIndex] = storm::utility::zero<ValueType>();
@@ -60,29 +60,18 @@ namespace storm {
                 // Stores objectives for which the current epoch passed their lower bound
                 storm::storage::BitVector lowerBoundViolatedObjectives(consideredObjectives.size(), false);
                 
-                auto lowerTimeBoundIt = lowerTimeBounds.begin();
-                auto upperTimeBoundIt = upperTimeBounds.begin();
-                uint_fast64_t currentEpoch = std::max(lowerTimeBounds.empty() ? 0 : lowerTimeBoundIt->first - 1, upperTimeBounds.empty() ? 0 : upperTimeBoundIt->first); // consider lowerBound - 1 since we are interested in the first epoch that passes the bound
+                auto stepBoundIt = stepBounds.begin();
+                uint_fast64_t currentEpoch = stepBounds.empty() ? 0 : stepBoundIt->first;
                 
-                while(currentEpoch > 0) {
-                    //For lower time bounds we need to react when the currentEpoch passed the bound
-                    // Hence, we substract 1 from the lower time bounds.
-                    if(lowerTimeBoundIt != lowerTimeBounds.end() && currentEpoch == lowerTimeBoundIt->first - 1) {
-                        lowerBoundViolatedObjectives |= lowerTimeBoundIt->second;
-                        for(auto objIndex : lowerTimeBoundIt->second) {
-                            // No more reward is earned for this objective.
-                            storm::utility::vector::addScaledVector(weightedRewardVector, this->discreteActionRewards[objIndex], -weightVector[objIndex]);
-                        }
-                        ++lowerTimeBoundIt;
-                    }
+                while (currentEpoch > 0) {
                     
-                    if(upperTimeBoundIt != upperTimeBounds.end() && currentEpoch == upperTimeBoundIt->first) {
-                        consideredObjectives |= upperTimeBoundIt->second;
-                        for(auto objIndex : upperTimeBoundIt->second) {
+                    if (stepBoundIt != stepBounds.end() && currentEpoch == stepBoundIt->first) {
+                        consideredObjectives |= stepBoundIt->second;
+                        for(auto objIndex : stepBoundIt->second) {
                             // This objective now plays a role in the weighted sum
                             storm::utility::vector::addScaledVector(weightedRewardVector, this->discreteActionRewards[objIndex], weightVector[objIndex]);
                         }
-                        ++upperTimeBoundIt;
+                        ++stepBoundIt;
                     }
                     
                     // Get values and scheduler for weighted sum of objectives
@@ -92,7 +81,7 @@ namespace storm {
                     
                     // get values for individual objectives
                     // TODO we could compute the result for one of the objectives from the weighted result, the given weight vector, and the remaining objective results.
-                    for(auto objIndex : consideredObjectives) {
+                    for (auto objIndex : consideredObjectives) {
                         std::vector<ValueType>& objectiveResult = this->objectiveResults[objIndex];
                         std::vector<ValueType> const& objectiveRewards = lowerBoundViolatedObjectives.get(objIndex) ? zeroReward : this->discreteActionRewards[objIndex];
                         auto rowGroupIndexIt = this->model.getTransitionMatrix().getRowGroupIndices().begin();
