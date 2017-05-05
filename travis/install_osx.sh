bb5a363689cb9f011cf725388f4dbbe5387584a9
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-05-05 13:37:02 +0200

    Use scripts from https://github.com/google/fruit

---
+++ travis/install_osx.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -x
+# Script installing dependencies
+# Inspired by https://github.com/google/fruit
+
+set -e
+
+# Helper for installing packages via homebrew
+install_brew_package() {
+  if brew list -1 | grep -q "^$1\$"; then
+    # Package is installed, upgrade if needed
+    brew outdated "$1" || brew upgrade "$@"
+  else
+    # Package not installed yet, install.
+    # If there are conflicts, try overwriting the files (these are in /usr/local anyway so it should be ok).
+    brew install "$@" || brew link --overwrite gcc49
+  fi
+}
+
+# Update packages
+brew update
+
+# For md5sum
+install_brew_package md5sha1sum
+# For `timeout'
+install_brew_package coreutils
+
+which cmake &>/dev/null || install_brew_package cmake
+
+# Install compiler
+case "${COMPILER}" in
+gcc-4.8)       install_brew_package gcc@4.8 ;;
+gcc-4.9)       install_brew_package gcc@4.9 ;;
+gcc-5)         install_brew_package gcc@5 ;;
+gcc-6)         install_brew_package gcc@6 ;;
+clang-default) ;;
+clang-3.7)     install_brew_package llvm@3.7 --with-clang --with-libcxx;;
+clang-3.8)     install_brew_package llvm@3.8 --with-clang --with-libcxx;;
+clang-3.9)     install_brew_package llvm@3.9 --with-clang --with-libcxx;;
+clang-4.0)     install_brew_package llvm     --with-clang --with-libcxx;;
+*) echo "Compiler not supported: ${COMPILER}. See travis_ci_install_osx.sh"; exit 1 ;;
+esac
+
+
+# Install dependencies
+install_brew_package gmp --c++11
+install_brew_package cln
+install_brew_package ginac
+install_brew_package doxygen
+install_brew_package boost --c++11
+brew tap homebrew/science
+install_brew_package homebrew/science/glpk
+install_brew_package homebrew/science/hwloc
---
+++ travis/postsubmit.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Inspired by https://github.com/google/fruit
+
+set -e
+
+: ${N_JOBS:=2}
+
+if [ "$STL" != "" ]
+then
+  STLARG="-stdlib=$STL"
+fi
+
+case $OS in
+linux)
+    echo "Linux currently unsupported"
+    exit 2
+#    docker rm -f fruit &>/dev/null || true
+#    docker run -d -it --name fruit --privileged polettimarco/fruit-basesystem:ubuntu-$UBUNTU
+#    docker exec fruit mkdir fruit
+#    docker cp . fruit:/fruit
+#
+#    docker exec fruit bash -c "
+#        export COMPILER=$COMPILER;
+#        export N_JOBS=$N_JOBS;
+#        export STLARG=$STLARG;
+#        export ASAN_OPTIONS=$ASAN_OPTIONS;
+#        export OS=$OS;
+#        cd fruit; extras/scripts/postsubmit-helper.sh $1"
+#    exit $?
+    ;;
+
+osx)
+    export COMPILER
+    export N_JOBS
+    export STLARG
+    export ASAN_OPTIONS
+    export OS
+    extras/scripts/postsubmit-helper.sh "$@"
+    exit $?
+    ;;
+
+*)
+    echo "Unsupported OS: $OS"
+    exit 1
+esac
