35573993366297d86198c1639be0f23aceb2be39
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-05-10 18:17:00 +0200

    Support for Debian 9

--- travis/dockerfiles/build_docker.sh
+++ travis/dockerfiles/build_docker.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# Build Ubuntu 16.10 "Yakkety Yak"
 docker build -t mvolk/storm-basesystem:ubuntu-16.10 -f Dockerfile.ubuntu-16.10 .
 docker push mvolk/storm-basesystem:ubuntu-16.10
+
+# Build Debian 9 "Stretch"
+docker build -t mvolk/storm-basesystem:debian-9 -f Dockerfile.debian-9 .
+docker push mvolk/storm-basesystem:debian-9
