b3c8b92ea5ca1634ad94ec4aab388097ebee6488
TimQu <tim.quatmann@cs.rwth-aachen.de>
2017-06-23 09:39:05 +0200

    added model transformations to the api

--- src/storm/transformer/ContinuousToDiscreteTimeModelTransformer.cpp
+++ src/storm/transformer/ContinuousToDiscreteTimeModelTransformer.cpp
@@ -14,72 +14,10 @@
 
 namespace storm {
     namespace transformer {
-        
-        template <typename ValueType, typename RewardModelType>
-        std::shared_ptr<storm::models::sparse::Model<ValueType>> transformContinuousToDiscreteModel(std::shared_ptr<storm::models::sparse::Model<ValueType, RewardModelType>> markovModel, std::shared_ptr<storm::logic::Formula const>& formula) {
-            boost::optional<std::string> timeRewardModelName;
-            if (formula->isTimeOperatorFormula()) {
-                auto const& timeOpFormula = formula->asTimeOperatorFormula();
-                if (timeOpFormula.getSubformula().isReachabilityTimeFormula()) {
-                    auto reachabilityRewardFormula = std::make_shared<storm::logic::EventuallyFormula>(storm::logic::CloneVisitor().clone(timeOpFormula.getSubformula().asReachabilityTimeFormula().getSubformula()), storm::logic::FormulaContext::Reward);
-                    timeRewardModelName = "time";
-                    // make sure that the reward model name is not already in use
-                    while (markovModel->hasRewardModel(*timeRewardModelName)) *timeRewardModelName += "_";
-                    formula = std::make_shared<storm::logic::RewardOperatorFormula const>(reachabilityRewardFormula, timeRewardModelName, timeOpFormula.getOperatorInformation());
-                }
-            }
-        
-            if (markovModel->isOfType(storm::models::ModelType::Ctmc)) {
-                SparseCtmcToSparseDtmcTransformer<ValueType, RewardModelType> transformer;
-                if (transformer.transformationPreservesProperty(*formula)) {
-                    STORM_LOG_INFO("Transforming Ctmc to embedded Dtmc...");
-                    return transformer.translate(*markovModel->template as<storm::models::sparse::Ctmc<ValueType>>(), timeRewardModelName);
-                }
-            } else if (markovModel->isOfType(storm::models::ModelType::MarkovAutomaton)) {
-                SparseMaToSparseMdpTransformer<ValueType, RewardModelType> transformer;
-                if (transformer.transformationPreservesProperty(*formula)) {
-                    STORM_LOG_INFO("Transforming Markov automaton to embedded Mdp...");
-                    return transformer.translate(*markovModel->template as<storm::models::sparse::MarkovAutomaton<ValueType>>(), timeRewardModelName);
-                }
-            } else {
-                STORM_LOG_THROW(false, storm::exceptions::UnexpectedException, "Model type " << markovModel->getType() << " not expected.");
-            }
-            return nullptr;
-        }
+
         
        template <typename ValueType, typename RewardModelType>
-        void transformContinuousToDiscreteModelInPlace(std::shared_ptr<storm::models::sparse::Model<ValueType, RewardModelType>>& markovModel, std::shared_ptr<storm::logic::Formula const>& formula) {
-            boost::optional<std::string> timeRewardModelName;
-            if (formula->isTimeOperatorFormula()) {
-                auto const& timeOpFormula = formula->asTimeOperatorFormula();
-                if (timeOpFormula.getSubformula().isReachabilityTimeFormula()) {
-                    auto reachabilityRewardFormula = std::make_shared<storm::logic::EventuallyFormula>(storm::logic::CloneVisitor().clone(timeOpFormula.getSubformula().asReachabilityTimeFormula().getSubformula()), storm::logic::FormulaContext::Reward);
-                    timeRewardModelName = "time";
-                    // make sure that the reward model name is not already in use
-                    while (markovModel->hasRewardModel(*timeRewardModelName)) *timeRewardModelName += "_";
-                    formula = std::make_shared<storm::logic::RewardOperatorFormula const>(reachabilityRewardFormula, timeRewardModelName, timeOpFormula.getOperatorInformation());
-                }
-            }
-        
-            if (markovModel->isOfType(storm::models::ModelType::Ctmc)) {
-                SparseCtmcToSparseDtmcTransformer<ValueType, RewardModelType> transformer;
-                if (transformer.transformationPreservesProperty(*formula)) {
-                    STORM_LOG_INFO("Transforming Ctmc to embedded Dtmc...");
-                    markovModel = transformer.translate(std::move(*markovModel->template as<storm::models::sparse::Ctmc<ValueType>>()), timeRewardModelName);
-                }
-            } else if (markovModel->isOfType(storm::models::ModelType::MarkovAutomaton)) {
-                SparseMaToSparseMdpTransformer<ValueType, RewardModelType> transformer;
-                if (transformer.transformationPreservesProperty(*formula)) {
-                    STORM_LOG_INFO("Transforming Markov automaton to embedded Mdp...");
-                    markovModel = transformer.translate(std::move(*markovModel->template as<storm::models::sparse::MarkovAutomaton<ValueType>>()), timeRewardModelName);
-                }
-            } else {
-                STORM_LOG_THROW(false, storm::exceptions::UnexpectedException, "Model type " << markovModel->getType() << " not expected.");
-            }
-        }
-        
-       template <typename ValueType, typename RewardModelType>
-        std::shared_ptr<storm::models::sparse::Dtmc<ValueType, RewardModelType>> SparseCtmcToSparseDtmcTransformer<ValueType, RewardModelType>::translate(storm::models::sparse::Ctmc<ValueType, RewardModelType> const& ctmc, boost::optional<std::string> const& timeRewardModelName) {
+        std::shared_ptr<storm::models::sparse::Dtmc<ValueType, RewardModelType>> ContinuousToDiscreteTimeModelTransformer<ValueType, RewardModelType>::transform(storm::models::sparse::Ctmc<ValueType, RewardModelType> const& ctmc, boost::optional<std::string> const& timeRewardModelName) {
             // Init the dtmc components
             storm::storage::sparse::ModelComponents<ValueType, RewardModelType> dtmcComponents(ctmc.getTransitionMatrix(), ctmc.getStateLabeling(), ctmc.getRewardModels());
             dtmcComponents.choiceLabeling = ctmc.getOptionalChoiceLabeling();
@@ -113,7 +51,7 @@ namespace storm {
         }
        
         template <typename ValueType, typename RewardModelType>
-        std::shared_ptr<storm::models::sparse::Dtmc<ValueType, RewardModelType>> SparseCtmcToSparseDtmcTransformer<ValueType, RewardModelType>::translate(storm::models::sparse::Ctmc<ValueType, RewardModelType>&& ctmc, boost::optional<std::string> const& timeRewardModelName) {
+        std::shared_ptr<storm::models::sparse::Dtmc<ValueType, RewardModelType>> ContinuousToDiscreteTimeModelTransformer<ValueType, RewardModelType>::transform(storm::models::sparse::Ctmc<ValueType, RewardModelType>&& ctmc, boost::optional<std::string> const& timeRewardModelName) {
             // Init the dtmc components
             storm::storage::sparse::ModelComponents<ValueType, RewardModelType> dtmcComponents(std::move(ctmc.getTransitionMatrix()), std::move(ctmc.getStateLabeling()), std::move(ctmc.getRewardModels()));
             dtmcComponents.choiceLabeling = std::move(ctmc.getOptionalChoiceLabeling());
@@ -144,7 +82,7 @@ namespace storm {
         }
        
         template <typename ValueType, typename RewardModelType>
-        bool SparseCtmcToSparseDtmcTransformer<ValueType, RewardModelType>::transformationPreservesProperty(storm::logic::Formula const& formula) {
+        bool ContinuousToDiscreteTimeModelTransformer<ValueType, RewardModelType>::preservesFormula(storm::logic::Formula const& formula) {
             storm::logic::FragmentSpecification fragment = storm::logic::propositional();
             fragment.setProbabilityOperatorsAllowed(true);
             fragment.setGloballyFormulasAllowed(true);
@@ -158,7 +96,7 @@ namespace storm {
         
   
         template <typename ValueType, typename RewardModelType>
-        std::shared_ptr<storm::models::sparse::Mdp<ValueType, RewardModelType>> SparseMaToSparseMdpTransformer<ValueType, RewardModelType>::translate(storm::models::sparse::MarkovAutomaton<ValueType, RewardModelType> const& ma, boost::optional<std::string> const& timeRewardModelName) {
+        std::shared_ptr<storm::models::sparse::Mdp<ValueType, RewardModelType>> ContinuousToDiscreteTimeModelTransformer<ValueType, RewardModelType>::transform(storm::models::sparse::MarkovAutomaton<ValueType, RewardModelType> const& ma, boost::optional<std::string> const& timeRewardModelName) {
             STORM_LOG_THROW(ma.isClosed(), storm::exceptions::InvalidArgumentException, "Transformation of MA to its underlying MDP is only possible for closed MAs");
 
             // Init the mdp components
@@ -195,7 +133,7 @@ namespace storm {
         }
 
         template <typename ValueType, typename RewardModelType>
-        std::shared_ptr<storm::models::sparse::Mdp<ValueType, RewardModelType>> SparseMaToSparseMdpTransformer<ValueType, RewardModelType>::translate(storm::models::sparse::MarkovAutomaton<ValueType, RewardModelType>&& ma, boost::optional<std::string> const& timeRewardModelName) {
+        std::shared_ptr<storm::models::sparse::Mdp<ValueType, RewardModelType>> ContinuousToDiscreteTimeModelTransformer<ValueType, RewardModelType>::transform(storm::models::sparse::MarkovAutomaton<ValueType, RewardModelType>&& ma, boost::optional<std::string> const& timeRewardModelName) {
             STORM_LOG_THROW(ma.isClosed(), storm::exceptions::InvalidArgumentException, "Transformation of MA to its underlying MDP is only possible for closed MAs");
             std::vector<ValueType>& exitRates = ma.getExitRates();
 
@@ -231,31 +169,8 @@ namespace storm {
             return std::make_shared<storm::models::sparse::Mdp<ValueType, RewardModelType>>(std::move(mdpComponents));
         }
         
-        template <typename ValueType, typename RewardModelType>
-        bool SparseMaToSparseMdpTransformer<ValueType, RewardModelType>::transformationPreservesProperty(storm::logic::Formula const& formula) {
-            storm::logic::FragmentSpecification fragment = storm::logic::propositional();
-            fragment.setProbabilityOperatorsAllowed(true);
-            fragment.setGloballyFormulasAllowed(true);
-            fragment.setReachabilityProbabilityFormulasAllowed(true);
-            fragment.setNextFormulasAllowed(true);
-            fragment.setUntilFormulasAllowed(true);
-            fragment.setRewardOperatorsAllowed(true);
-            fragment.setReachabilityRewardFormulasAllowed(true);
-            
-            return formula.isInFragment(fragment);
-        }
-    
-        template std::shared_ptr<storm::models::sparse::Model<double>> transformContinuousToDiscreteModel(std::shared_ptr<storm::models::sparse::Model<double>> markovModel, std::shared_ptr<storm::logic::Formula const>& formula);
-        template std::shared_ptr<storm::models::sparse::Model<storm::RationalNumber>> transformContinuousToDiscreteModel(std::shared_ptr<storm::models::sparse::Model<storm::RationalNumber>> markovModel, std::shared_ptr<storm::logic::Formula const>& formula);
-        template std::shared_ptr<storm::models::sparse::Model<storm::RationalFunction>> transformContinuousToDiscreteModel(std::shared_ptr<storm::models::sparse::Model<storm::RationalFunction>> markovModel, std::shared_ptr<storm::logic::Formula const>& formula);
-        template void transformContinuousToDiscreteModelInPlace<double>(std::shared_ptr<storm::models::sparse::Model<double>>& markovModel, std::shared_ptr<storm::logic::Formula const>& formula);
-        template void transformContinuousToDiscreteModelInPlace<storm::RationalNumber>(std::shared_ptr<storm::models::sparse::Model<storm::RationalNumber>>& markovModel, std::shared_ptr<storm::logic::Formula const>& formula);
-        template void transformContinuousToDiscreteModelInPlace<storm::RationalFunction>(std::shared_ptr<storm::models::sparse::Model<storm::RationalFunction>>& markovModel, std::shared_ptr<storm::logic::Formula const>& formula);
-        template class SparseCtmcToSparseDtmcTransformer<double>;
-        template class SparseCtmcToSparseDtmcTransformer<storm::RationalNumber>;
-        template class SparseCtmcToSparseDtmcTransformer<storm::RationalFunction>;
-        template class SparseMaToSparseMdpTransformer<double>;
-        template class SparseMaToSparseMdpTransformer<storm::RationalNumber>;
-        template class SparseMaToSparseMdpTransformer<storm::RationalFunction>;
+        template class ContinuousToDiscreteTimeModelTransformer<double>;
+        template class ContinuousToDiscreteTimeModelTransformer<storm::RationalNumber>;
+        template class ContinuousToDiscreteTimeModelTransformer<storm::RationalFunction>;
     }
 }
