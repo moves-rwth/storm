53a2723e0cce7dd4fc9c8cb3e9f4e69956ec7b41
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2017-07-11 22:48:09 +0200

    storm pars result moved from storm to storm pars

--- src/storm/api/export.h
+++ src/storm/api/export.h
@@ -2,7 +2,6 @@
 
 #include "storm/storage/jani/JSONExporter.h"
 
-#include "storm/analysis/GraphConditions.h"
 
 #include "storm/settings/SettingsManager.h"
 #include "storm/settings/modules/JaniExportSettings.h"
@@ -10,7 +9,6 @@
 #include "storm/utility/DirectEncodingExporter.h"
 #include "storm/utility/file.h"
 #include "storm/utility/macros.h"
-#include "storm/exceptions/NotSupportedException.h"
 
 namespace storm {
     namespace api {
@@ -18,32 +16,7 @@ namespace storm {
         void exportJaniModel(storm::jani::Model const& model, std::vector<storm::jani::Property> const& properties, std::string const& filename);
         
         void exportJaniModelAsDot(storm::jani::Model const& model, std::string const& filename);
-        
-        template <typename ValueType>
-        void exportParametricResultToFile(ValueType const& result, storm::analysis::ConstraintCollector<ValueType> const& constraintCollector, std::string const& path) {
-            STORM_LOG_THROW(false, storm::exceptions::NotSupportedException, "Cannot export non-parametric result.");
-        }
-        
-        template <>
-        inline void exportParametricResultToFile(storm::RationalFunction const& result, storm::analysis::ConstraintCollector<storm::RationalFunction> const& constraintCollector, std::string const& path) {
-            std::ofstream filestream;
-            storm::utility::openFile(path, filestream);
-            filestream << "!Parameters: ";
-            std::set<storm::RationalFunctionVariable> vars = result.gatherVariables();
-            std::copy(vars.begin(), vars.end(), std::ostream_iterator<storm::RationalFunctionVariable>(filestream, "; "));
-            filestream << std::endl;
-            filestream << "!Result: " << result.toString(false, true) << std::endl;
-            filestream << "!Well-formed Constraints: " << std::endl;
-            std::vector<std::string> stringConstraints;
-            std::transform(constraintCollector.getWellformedConstraints().begin(), constraintCollector.getWellformedConstraints().end(), std::back_inserter(stringConstraints), [](carl::Formula<typename storm::Polynomial::PolyType> const& c) ->  std::string { return c.toString();});
-            std::copy(stringConstraints.begin(), stringConstraints.end(), std::ostream_iterator<std::string>(filestream, "\n"));
-            filestream << "!Graph-preserving Constraints: " << std::endl;
-            stringConstraints.clear();
-            std::transform(constraintCollector.getGraphPreservingConstraints().begin(), constraintCollector.getGraphPreservingConstraints().end(), std::back_inserter(stringConstraints), [](carl::Formula<typename storm::Polynomial::PolyType> const& c) ->  std::string { return c.toString();});
-            std::copy(stringConstraints.begin(), stringConstraints.end(), std::ostream_iterator<std::string>(filestream, "\n"));
-            storm::utility::closeFile(filestream);
-        }
-        
+
         template <typename ValueType>
         void exportSparseModelAsDrn(std::shared_ptr<storm::models::sparse::Model<ValueType>> const& model, std::string const& filename, std::vector<std::string> const& parameterNames) {
             std::ofstream stream;
