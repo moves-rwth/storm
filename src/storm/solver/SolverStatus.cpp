af60db2c38338876dc11b896cbcd169714b7b0ce
dehnert <dehnert@cs.rwth-aachen.de>
2017-10-10 15:53:56 +0200

    work towards symbolic rational search

---
+++ src/storm/solver/SolverStatus.cpp
@@ -0,0 +1,8 @@
+//
+//  SolverStatus.cpp
+//  storm
+//
+//  Created by Christian Dehnert on 10.10.17.
+//
+
+#include "SolverStatus.hpp"
---
+++ src/storm/solver/SolverStatus.h
@@ -0,0 +1,13 @@
+//
+//  SolverStatus.hpp
+//  storm
+//
+//  Created by Christian Dehnert on 10.10.17.
+//
+
+#ifndef SolverStatus_hpp
+#define SolverStatus_hpp
+
+#include <stdio.h>
+
+#endif /* SolverStatus_hpp */
