c7d9b0b61dfa2f25158edf86889467aa555a2148
TimQu <tim.quatmann@cs.rwth-aachen.de>
2017-10-12 21:29:07 +0200

    Fixed that bisimulation did not preserve reward bounded formulas

--- src/storm/logic/FormulaInformation.cpp
+++ src/storm/logic/FormulaInformation.cpp
@@ -20,11 +20,21 @@ namespace storm {
             return this->mContainsBoundedUntilFormula;
         }
         
+        bool FormulaInformation::containsCumulativeRewardFormula() const {
+            return this->mContainsCumulativeRewardFormula;
+        }
+        
+        bool FormulaInformation::containsRewardBoundedFormula() const {
+            return this->mContainsRewardBoundedFormula;
+        }
+        
         FormulaInformation FormulaInformation::join(FormulaInformation const& other) {
             FormulaInformation result;
             result.mContainsRewardOperator = this->containsRewardOperator() || other.containsRewardOperator();
             result.mContainsNextFormula = this->containsNextFormula() || other.containsNextFormula();
             result.mContainsBoundedUntilFormula = this->containsBoundedUntilFormula() || other.containsBoundedUntilFormula();
+            result.mContainsCumulativeRewardFormula = this->containsCumulativeRewardFormula() || other.containsCumulativeRewardFormula();
+            result.mContainsRewardBoundedFormula = this->containsRewardBoundedFormula() || other.containsRewardBoundedFormula();
             return result;
         }
         
@@ -40,6 +50,16 @@ namespace storm {
         
         FormulaInformation& FormulaInformation::setContainsBoundedUntilFormula(bool newValue) {
             this->mContainsBoundedUntilFormula = newValue;
+            return *this;
+        }
+        
+        FormulaInformation& FormulaInformation::setContainsCumulativeRewardFormula(bool newValue) {
+            this->mContainsCumulativeRewardFormula = newValue;
+            return *this;
+        }
+        
+        FormulaInformation& FormulaInformation::setContainsRewardBoundedFormula(bool newValue) {
+            this->mContainsRewardBoundedFormula = newValue;
             return *this;
         }
     }
