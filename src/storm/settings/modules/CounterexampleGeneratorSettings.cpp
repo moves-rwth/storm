b3c2d8bbd8b04961e35c9cecc629908c3f5a4c78
dehnert <dehnert@cs.rwth-aachen.de>
2018-03-09 10:35:14 +0100

    added option to not include dynamic constraints in maxsat counterexample generation

--- src/storm/settings/modules/CounterexampleGeneratorSettings.cpp
+++ src/storm/settings/modules/CounterexampleGeneratorSettings.cpp
@@ -16,13 +16,15 @@ namespace storm {
             const std::string CounterexampleGeneratorSettings::minimalCommandSetOptionName = "mincmd";
             const std::string CounterexampleGeneratorSettings::encodeReachabilityOptionName = "encreach";
             const std::string CounterexampleGeneratorSettings::schedulerCutsOptionName = "schedcuts";
-            
+            const std::string CounterexampleGeneratorSettings::noDynamicConstraintsOptionName = "nodyn";
+
             CounterexampleGeneratorSettings::CounterexampleGeneratorSettings() : ModuleSettings(moduleName) {
                 std::vector<std::string> techniques = {"maxsat", "milp"};
-                this->addOption(storm::settings::OptionBuilder(moduleName, minimalCommandSetOptionName, true, "Computes a counterexample for the given model in terms of a minimal command set. Note that this requires the model to be given in a symbolic format.")
+                this->addOption(storm::settings::OptionBuilder(moduleName, minimalCommandSetOptionName, true, "Computes a counterexample for the given model in terms of a minimal command/edge set. Note that this requires the model to be given in a symbolic format.")
                                 .addArgument(storm::settings::ArgumentBuilder::createStringArgument("method", "Sets which technique is used to derive the counterexample.").setDefaultValueString("maxsat").addValidatorString(ArgumentValidatorFactory::createMultipleChoiceValidator(techniques)).build()).build());
-                this->addOption(storm::settings::OptionBuilder(moduleName, encodeReachabilityOptionName, true, "Sets whether to encode reachability for MAXSAT-based minimal command counterexample generation.").build());
-                this->addOption(storm::settings::OptionBuilder(moduleName, schedulerCutsOptionName, true, "Sets whether to add the scheduler cuts for MILP-based minimal command counterexample generation.").build());
+                this->addOption(storm::settings::OptionBuilder(moduleName, encodeReachabilityOptionName, true, "Sets whether to encode reachability for MAXSAT-based counterexample generation.").build());
+                this->addOption(storm::settings::OptionBuilder(moduleName, schedulerCutsOptionName, true, "Sets whether to add the scheduler cuts for MILP-based counterexample generation.").build());
+                this->addOption(storm::settings::OptionBuilder(moduleName, noDynamicConstraintsOptionName, true, "Disables the generation of dynamic constraints in the MAXSAT-based counterexample generation.").build());
             }
             
             bool CounterexampleGeneratorSettings::isMinimalCommandSetGenerationSet() const {
@@ -44,7 +46,11 @@ namespace storm {
             bool CounterexampleGeneratorSettings::isUseSchedulerCutsSet() const {
                 return this->getOption(schedulerCutsOptionName).getHasOptionBeenSet();
             }
-            
+
+            bool CounterexampleGeneratorSettings::isUseDynamicConstraintsSet() const {
+                return !this->getOption(noDynamicConstraintsOptionName).getHasOptionBeenSet();
+            }
+
             bool CounterexampleGeneratorSettings::check() const {
                 // Ensure that the model was given either symbolically or explicitly.
                 STORM_LOG_THROW(!isMinimalCommandSetGenerationSet() || storm::settings::getModule<storm::settings::modules::IOSettings>().isPrismInputSet(), storm::exceptions::InvalidSettingsException, "For the generation of a minimal command set, the model has to be specified in the PRISM format.");
