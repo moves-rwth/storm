415e22743d488888582b0f7b8f85d58f39e66bd4
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2018-03-20 18:01:35 +0100

    Moved same parts of the dft api into cpp file

--- src/storm-dft/api/storm-dft.h
+++ src/storm-dft/api/storm-dft.h
@@ -83,7 +83,6 @@ namespace storm {
             return results;
         }
 
-
         /*!
          * Export DFT to JSON file.
          *
@@ -91,20 +90,7 @@ namespace storm {
          * @param file File.
          */
         template<typename ValueType>
-        typename std::enable_if<std::is_same<ValueType, double>::value, void>::type exportDFTToJson(storm::storage::DFT<ValueType> const& dft, std::string const& file) {
-            storm::storage::DftJsonExporter<ValueType>::toFile(dft, file);
-        }
-
-        /*!
-         * Export DFT to JSON file.
-         *
-         * @param dft DFT.
-         * @param file File.
-         */
-        template<typename ValueType>
-        typename std::enable_if<!std::is_same<ValueType, double>::value, void>::type exportDFTToJson(storm::storage::DFT<ValueType> const& dft, std::string const& file) {
-            STORM_LOG_THROW(false, storm::exceptions::NotSupportedException, "Export to JSON not supported for this data type.");
-        }
+        void exportDFTToJson(storm::storage::DFT<ValueType> const& dft, std::string const& file);
 
         /*!
          * Export DFT to SMT encoding.
@@ -113,22 +99,7 @@ namespace storm {
          * @param file File.
          */
         template<typename ValueType>
-        typename std::enable_if<std::is_same<ValueType, double>::value, void>::type exportDFTToSMT(storm::storage::DFT<ValueType> const& dft, std::string const& file) {
-            storm::modelchecker::DFTASFChecker asfChecker(dft);
-            asfChecker.convert();
-            asfChecker.toFile(file);
-        }
-
-        /*!
-         * Export DFT to SMT encoding.
-         *
-         * @param dft DFT.
-         * @param file File.
-         */
-        template<typename ValueType>
-        typename std::enable_if<!std::is_same<ValueType, double>::value, void>::type exportDFTToSMT(storm::storage::DFT<ValueType> const& dft, std::string const& file) {
-            STORM_LOG_THROW(false, storm::exceptions::NotSupportedException, "Export to SMT does not support this data type.");
-        }
+        void exportDFTToSMT(storm::storage::DFT<ValueType> const& dft, std::string const& file);
 
         /*!
          * Transform DFT to GSPN.
@@ -136,47 +107,7 @@ namespace storm {
          * @param dft DFT.
          */
         template<typename ValueType>
-        typename std::enable_if<std::is_same<ValueType, double>::value, void>::type transformToGSPN(storm::storage::DFT<ValueType> const& dft) {
-            // Transform to GSPN
-            storm::transformations::dft::DftToGspnTransformator<double> gspnTransformator(dft);
-            bool smart = true;
-            gspnTransformator.transform(smart);
-            storm::gspn::GSPN* gspn = gspnTransformator.obtainGSPN();
-            uint64_t toplevelFailedPlace = gspnTransformator.toplevelFailedPlaceId();
-
-            storm::api::handleGSPNExportSettings(*gspn);
-
-            std::shared_ptr<storm::expressions::ExpressionManager> const& exprManager = gspn->getExpressionManager();
-            storm::builder::JaniGSPNBuilder builder(*gspn);
-            storm::jani::Model* model =  builder.build();
-            storm::jani::Variable const& topfailedVar = builder.getPlaceVariable(toplevelFailedPlace);
-
-            storm::expressions::Expression targetExpression = exprManager->integer(1) == topfailedVar.getExpressionVariable().getExpression();
-            auto evtlFormula = std::make_shared<storm::logic::AtomicExpressionFormula>(targetExpression);
-            auto tbFormula = std::make_shared<storm::logic::BoundedUntilFormula>(std::make_shared<storm::logic::BooleanLiteralFormula>(true), evtlFormula, storm::logic::TimeBound(false, exprManager->integer(0)), storm::logic::TimeBound(false, exprManager->integer(10)), storm::logic::TimeBoundReference(storm::logic::TimeBoundType::Time));
-            auto tbUntil = std::make_shared<storm::logic::ProbabilityOperatorFormula>(tbFormula);
-
-            auto evFormula = std::make_shared<storm::logic::EventuallyFormula>(evtlFormula, storm::logic::FormulaContext::Time);
-            auto rewFormula = std::make_shared<storm::logic::TimeOperatorFormula>(evFormula, storm::logic::OperatorInformation(), storm::logic::RewardMeasureType::Expectation);
-
-            storm::settings::modules::JaniExportSettings const& janiSettings = storm::settings::getModule<storm::settings::modules::JaniExportSettings>();
-            if (janiSettings.isJaniFileSet()) {
-                storm::api::exportJaniModel(*model, {storm::jani::Property("time-bounded", tbUntil), storm::jani::Property("mttf", rewFormula)}, janiSettings.getJaniFilename());
-            }
-
-            delete model;
-            delete gspn;
-        }
-
-        /*!
-         * Transform DFT to GSPN.
-         *
-         * @param dft DFT.
-         */
-        template<typename ValueType>
-        typename std::enable_if<!std::is_same<ValueType, double>::value, void>::type transformToGSPN(storm::storage::DFT<ValueType> const& dft) {
-            STORM_LOG_THROW(false, storm::exceptions::NotSupportedException, "Transformation to GSPN not supported for this data type.");
-        }
+        void transformToGSPN(storm::storage::DFT<ValueType> const& dft);
 
     }
 }
