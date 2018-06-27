571e157eeff40f3a5098a4e7b30a8699f12a80b8
TimQu <tim.quatmann@cs.rwth-aachen.de>
2018-06-27 15:48:15 +0200

    added missing return statement

--- src/storm/modelchecker/multiobjective/deterministicScheds/DetSchedsWeightVectorChecker.cpp
+++ src/storm/modelchecker/multiobjective/deterministicScheds/DetSchedsWeightVectorChecker.cpp
@@ -66,7 +66,7 @@ namespace storm {
                         }
                     }
                 } while (!schedulerEvaluator->hasCurrentSchedulerBeenChecked());
-                
+                return resultStack;
             }
             
             template <typename ModelType>
