19c6472843d695151a4d752a0b3531403f920e51
TimQu <tim.quatmann@cs.rwth-aachen.de>
2018-08-09 08:14:20 +0200

    separating declaration/implementation for storm-conv api

---
+++ src/storm-conv/api/storm-conv.cpp
@@ -0,0 +1,58 @@
+#include "storm-conv/api/storm-conv.h"
+
+#include "storm/storage/prism/Program.h"
+#include "storm/storage/jani/Property.h"
+#include "storm/storage/jani/JaniLocationExpander.h"
+#include "storm/storage/jani/JSONExporter.h"
+
+namespace storm {
+    namespace api {
+        
+        void postprocessJani(storm::jani::Model& janiModel, storm::converter::JaniConversionOptions options) {
+        
+            if (!options.locationVariables.empty()) {
+                for (auto const& pair : options.locationVariables) {
+                    storm::jani::JaniLocationExpander expander(janiModel);
+                    expander.transform(pair.first, pair.second);
+                    janiModel = expander.getResult();
+                }
+            }
+
+            if (options.exportFlattened) {
+                janiModel = janiModel.flattenComposition();
+            }
+
+            if (options.standardCompliant) {
+                janiModel.makeStandardJaniCompliant();
+            }
+        }
+        
+        std::pair<storm::jani::Model, std::vector<storm::jani::Property>> convertPrismToJani(storm::prism::Program const& program, std::vector<storm::jani::Property> const& properties, storm::converter::PrismToJaniConverterOptions options) {
+            std::pair<storm::jani::Model, std::vector<storm::jani::Property>> res;
+        
+            // Perform conversion
+            auto modelAndRenaming = program.toJaniWithLabelRenaming(options.allVariablesGlobal, options.suffix, options.janiOptions.standardCompliant);
+            res.first = std::move(modelAndRenaming.first);
+        
+            // Amend properties to potentially changed labels
+            for (auto const& property : properties) {
+                res.second.emplace_back(property.substituteLabels(modelAndRenaming.second));
+            }
+            
+            // Postprocess Jani model based on the options
+            postprocessJani(res.first, options.janiOptions);
+            
+            return res;
+        }
+        
+        void exportJaniToFile(storm::jani::Model const& model, std::vector<storm::jani::Property> const& properties, std::string const& filename) {
+            storm::jani::JsonExporter::toFile(model, properties, filename);
+        }
+        
+        void printJaniToStream(storm::jani::Model const& model, std::vector<storm::jani::Property> const& properties, std::ostream& ostream) {
+            storm::jani::JsonExporter::toStream(model, properties, ostream);
+        }
+
+        
+    }
+}
--- src/storm-conv/api/storm-conv.h
+++ src/storm-conv/api/storm-conv.h
@@ -3,58 +3,25 @@
 #include "storm-conv/converter/options/PrismToJaniConverterOptions.h"
 #include "storm-conv/converter/options/JaniConversionOptions.h"
 
-#include "storm/storage/prism/Program.h"
-#include "storm/storage/jani/Property.h"
-#include "storm/storage/jani/JaniLocationExpander.h"
-#include "storm/storage/jani/JSONExporter.h"
-
 namespace storm {
+    
+    namespace prism {
+        class Program;
+    }
+    namespace jani {
+        class Model;
+        class Property;
+    }
+
     namespace api {
         
-        void postprocessJani(storm::jani::Model& janiModel, storm::converter::JaniConversionOptions options) {
-        
-            if (!options.locationVariables.empty()) {
-                for (auto const& pair : options.locationVariables) {
-                    storm::jani::JaniLocationExpander expander(janiModel);
-                    expander.transform(pair.first, pair.second);
-                    janiModel = expander.getResult();
-                }
-            }
+        void postprocessJani(storm::jani::Model& janiModel, storm::converter::JaniConversionOptions options);
 
-            if (options.exportFlattened) {
-                janiModel = janiModel.flattenComposition();
-            }
-
-            if (options.standardCompliant) {
-                janiModel.makeStandardJaniCompliant();
-            }
-        }
+        std::pair<storm::jani::Model, std::vector<storm::jani::Property>> convertPrismToJani(storm::prism::Program const& program, std::vector<storm::jani::Property> const& properties = std::vector<storm::jani::Property>(), storm::converter::PrismToJaniConverterOptions options = storm::converter::PrismToJaniConverterOptions());
         
-        std::pair<storm::jani::Model, std::vector<storm::jani::Property>> convertPrismToJani(storm::prism::Program const& program, std::vector<storm::jani::Property> const& properties = std::vector<storm::jani::Property>(), storm::converter::PrismToJaniConverterOptions options = storm::converter::PrismToJaniConverterOptions()) {
-            std::pair<storm::jani::Model, std::vector<storm::jani::Property>> res;
+        void exportJaniToFile(storm::jani::Model const& model, std::vector<storm::jani::Property> const& properties, std::string const& filename);
         
-            // Perform conversion
-            auto modelAndRenaming = program.toJaniWithLabelRenaming(options.allVariablesGlobal, options.suffix, options.janiOptions.standardCompliant);
-            res.first = std::move(modelAndRenaming.first);
-        
-            // Amend properties to potentially changed labels
-            for (auto const& property : properties) {
-                res.second.emplace_back(property.substituteLabels(modelAndRenaming.second));
-            }
-            
-            // Postprocess Jani model based on the options
-            postprocessJani(res.first, options.janiOptions);
-            
-            return res;
-        }
-        
-        void exportJaniToFile(storm::jani::Model const& model, std::vector<storm::jani::Property> const& properties, std::string const& filename) {
-            storm::jani::JsonExporter::toFile(model, properties, filename);
-        }
-        
-        void printJaniToStream(storm::jani::Model const& model, std::vector<storm::jani::Property> const& properties, std::ostream& ostream) {
-            storm::jani::JsonExporter::toStream(model, properties, ostream);
-        }
+        void printJaniToStream(storm::jani::Model const& model, std::vector<storm::jani::Property> const& properties, std::ostream& ostream);
 
         
     }
