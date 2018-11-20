7e61ab4a0fd7f96f388944e2a4aa6d04afa49db5
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2018-11-20 17:31:58 +0100

    Travis: Disable deployment if no credentials are given

--- travis/deploy_carl.sh
+++ travis/deploy_carl.sh
@@ -4,26 +4,34 @@ set -e
 
 OS=$TRAVIS_OS_NAME
 
+# Do not deploy if credentials are not given
+if [ "${TRAVIS_SECURE_ENV_VARS}" == "false" ]; then
+    echo "WARNING: Not deploying as no credentials are given."
+    exit 0;
+fi
+
+# Do not deploy for pull requests
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    exit 0;
+fi
+
 case $OS in
 linux)
-    # Only deploy for non pull requests
-    if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-        # Deploy as debug/release
-        case "$CONFIG" in
-        *DebugTravis)
-            docker commit carl movesrwth/carl:travis-debug
-            docker push movesrwth/carl:travis-debug
-            ;;
-        *ReleaseTravis)
-            docker commit carl movesrwth/carl:travis
-            docker push movesrwth/carl:travis
-            ;;
-        *)
-            echo "Unrecognized value of CONFIG: $CONFIG"; exit 1
-            ;;
-        esac
-    fi
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    # Deploy as debug/release
+    case "$CONFIG" in
+    *DebugTravis)
+        docker commit carl movesrwth/carl:travis-debug
+        docker push movesrwth/carl:travis-debug
+        ;;
+    *ReleaseTravis)
+        docker commit carl movesrwth/carl:travis
+        docker push movesrwth/carl:travis
+        ;;
+    *)
+        echo "Unrecognized value of CONFIG: $CONFIG"; exit 1
+        ;;
+    esac
     ;;
 
 osx)
