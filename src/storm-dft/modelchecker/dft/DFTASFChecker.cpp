9c28ed990e50f92736c6d5cf604e77d352ef3e62
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2019-05-29 18:48:21 +0200

    Use isBasicElement() instead of type

--- src/storm-dft/modelchecker/dft/DFTASFChecker.cpp
+++ src/storm-dft/modelchecker/dft/DFTASFChecker.cpp
@@ -619,8 +619,7 @@ namespace storm {
             // Count dependent events
             for (size_t i = 0; i < dft.nrElements(); ++i) {
                 std::shared_ptr<storm::storage::DFTElement<ValueType> const> element = dft.getElement(i);
-                if (element->type() == storm::storage::DFTElementType::BE_EXP ||
-                    element->type() == storm::storage::DFTElementType::BE_CONST) {
+                if (element->isBasicElement()) {
                     auto be = std::static_pointer_cast<storm::storage::DFTBE<double> const>(element);
                     if (be->hasIngoingDependencies()) {
                         ++nrDepEvents;
