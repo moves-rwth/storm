1536edb99f612e20571388573efc899da63d7387
Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
2019-11-13 17:53:44 +0100

    Jani: Check if a variable is never used as the lvalue of an assignment. If yes, (and if the variable has a known initial value), we replace the variable by a constant.

--- src/storm/storage/jani/traverser/AssignmentsFinder.h
+++ src/storm/storage/jani/traverser/AssignmentsFinder.h
@@ -22,7 +22,9 @@ namespace storm {
             AssignmentsFinder() = default;
             
             ResultType find(Model const& model, storm::jani::Variable const& variable);
+            ResultType find(Automaton const& automaton, storm::jani::Variable const& variable);
             ResultType find(Model const& model, storm::expressions::Variable const& variable);
+            ResultType find(Automaton const& automaton, storm::expressions::Variable const& variable);
             
             virtual ~AssignmentsFinder() = default;
             
