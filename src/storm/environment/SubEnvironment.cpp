d88e7e9951c2f0c582b7e37fa0c029facbf78123
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2020-03-09 13:53:15 +0100

    Explicit header files to include all defined environments

--- src/storm/environment/SubEnvironment.cpp
+++ src/storm/environment/SubEnvironment.cpp
@@ -1,20 +1,9 @@
 #include <memory>
 
 #include "storm/environment/Environment.h"
-#include "storm/environment/modelchecker/MultiObjectiveModelCheckerEnvironment.h"
-#include "storm/environment/modelchecker/ModelCheckerEnvironment.h"
 
-#include "storm/environment/solver/SolverEnvironment.h"
-#include "storm/environment/solver/EigenSolverEnvironment.h"
-#include "storm/environment/solver/GameSolverEnvironment.h"
-#include "storm/environment/solver/GmmxxSolverEnvironment.h"
-#include "storm/environment/solver/NativeSolverEnvironment.h"
-#include "storm/environment/solver/LongRunAverageSolverEnvironment.h"
-#include "storm/environment/solver/TimeBoundedSolverEnvironment.h"
-#include "storm/environment/solver/MinMaxSolverEnvironment.h"
-#include "storm/environment/solver/MultiplierEnvironment.h"
-#include "storm/environment/solver/OviSolverEnvironment.h"
-#include "storm/environment/solver/TopologicalSolverEnvironment.h"
+#include "storm/environment/modelchecker/AllModelCheckerEnvironments.h"
+#include "storm/environment/solver/AllSolverEnvironments.h"
 
 namespace storm {
     
--- src/storm/environment/solver/SolverEnvironment.cpp
+++ src/storm/environment/solver/SolverEnvironment.cpp
@@ -1,15 +1,6 @@
 #include "storm/environment/solver/SolverEnvironment.h"
 
-#include "storm/environment/solver/LongRunAverageSolverEnvironment.h"
-#include "storm/environment/solver/TimeBoundedSolverEnvironment.h"
-#include "storm/environment/solver/MinMaxSolverEnvironment.h"
-#include "storm/environment/solver/MultiplierEnvironment.h"
-#include "storm/environment/solver/EigenSolverEnvironment.h"
-#include "storm/environment/solver/GmmxxSolverEnvironment.h"
-#include "storm/environment/solver/NativeSolverEnvironment.h"
-#include "storm/environment/solver/GameSolverEnvironment.h"
-#include "storm/environment/solver/TopologicalSolverEnvironment.h"
-#include "storm/environment/solver/OviSolverEnvironment.h"
+#include "storm/environment/solver/AllSolverEnvironments.h"
 
 #include "storm/settings/SettingsManager.h"
 #include "storm/settings/modules/GeneralSettings.h"
