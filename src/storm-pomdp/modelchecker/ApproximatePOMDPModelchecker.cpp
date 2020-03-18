581e165fb91b1d16658fd362eb74d7b711cc7221
Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
2020-03-18 13:59:46 +0100

    Actually use the refinement precision....

--- src/storm-pomdp/modelchecker/ApproximatePOMDPModelchecker.cpp
+++ src/storm-pomdp/modelchecker/ApproximatePOMDPModelchecker.cpp
@@ -219,7 +219,8 @@ namespace storm {
                     res = computeRefinementStep(targetObservations, min, observationResolutionVector, false,
                                                 res, changedObservations, initialOverApproxMap, underApproxMap, underApproxModelSize);
                     //storm::api::exportSparseModelAsDot(res->overApproxModelPtr, "oa_model_" + std::to_string(refinementCounter +1) + ".dot");
-                    if (cc.isEqual(res->overApproxValue, res->underApproxValue)) {
+                    STORM_LOG_ERROR_COND(cc.isLess(res->underApproxValue, res->overApproxValue), "The value for the under-approximation is larger than the value for the over-approximation.");
+                    if (res->overApproxValue - res->underApproxValue <= options.refinementPrecision) {
                         break;
                     }
                     ++refinementCounter;
