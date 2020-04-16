fddc1dde98039ebebbb483d32a3358bdf386454b
Daniel Basgöze <daniel.basgoeze@rwth-aachen.de>
2020-04-16 16:40:38 +0200

    Use InternalDdManager to initialize Sylvan.
    Fixes double init Sylvan and use after free issues.

--- src/storm-dft/storage/SylvanBddManager.h
+++ src/storm-dft/storage/SylvanBddManager.h
@@ -2,11 +2,7 @@
 
 #include <map>
 
-#include "storm-config.h"
-#include "storm/exceptions/InvalidSettingsException.h"
-#include "storm/exceptions/NotSupportedException.h"
-#include "storm/settings/SettingsManager.h"
-#include "storm/settings/modules/SylvanSettings.h"
+#include "storm/storage/dd/sylvan/InternalSylvanDdManager.h"
 #include "storm/utility/macros.h"
 #include "storm/utility/sylvan.h"
 
@@ -26,64 +22,12 @@ class SylvanBddManager {
    public:
     /**
      * Initilizes Sylvan
+     *
+     * \note
+     * Internally Sylvan is initialiesed by a InternalSylvanDdManager.
+     * This ensures compatibility.
      */
-    SylvanBddManager() {
-        storm::settings::modules::SylvanSettings const& settings =
-            storm::settings::getModule<
-                storm::settings::modules::SylvanSettings>();
-        if (settings.isNumberOfThreadsSet()) {
-            lace_init(settings.getNumberOfThreads(), 1024 * 1024 * 16);
-        } else {
-            lace_init(0, 1024 * 1024 * 16);
-        }
-        lace_startup(0, 0, 0);
-
-        // Table/cache size computation taken from newer version of sylvan.
-        uint64_t memorycap = storm::settings::getModule<
-                                 storm::settings::modules::SylvanSettings>()
-                                 .getMaximalMemory() *
-                             1024 * 1024;
-
-        uint64_t table_ratio = 0;
-        uint64_t initial_ratio = 0;
-
-        uint64_t max_t = 1;
-        uint64_t max_c = 1;
-        if (table_ratio > 0) {
-            max_t <<= table_ratio;
-        } else {
-            max_c <<= -table_ratio;
-        }
-
-        uint64_t cur = max_t * 24 + max_c * 36;
-        STORM_LOG_THROW(cur <= memorycap,
-                        storm::exceptions::InvalidSettingsException,
-                        "Memory cap incompatible with default table ratio.");
-        STORM_LOG_WARN_COND(memorycap < 60 * 0x0000040000000000,
-                            "Sylvan only supports tablesizes <= 42 bits. "
-                            "Memory limit is changed accordingly.");
-
-        while (2 * cur < memorycap && max_t < 0x0000040000000000) {
-            max_t *= 2;
-            max_c *= 2;
-            cur *= 2;
-        }
-
-        uint64_t min_t = max_t, min_c = max_c;
-        while (initial_ratio > 0 && min_t > 0x1000 && min_c > 0x1000) {
-            min_t >>= 1;
-            min_c >>= 1;
-            initial_ratio--;
-        }
-        // End of copied code.
-
-        STORM_LOG_DEBUG("Initializing sylvan library. Initial/max table size: "
-                        << min_t << "/" << max_t << ", initial/max cache size: "
-                        << min_c << "/" << max_c << ".");
-        sylvan::Sylvan::initPackage(min_t, max_t, min_c, max_c);
-
-        sylvan::Sylvan::initBdd();
-    }
+    SylvanBddManager() = default;
 
     // We can only initialize Sylvan once therefore no copy semantics
     SylvanBddManager(SylvanBddManager const&) = delete;
@@ -94,10 +38,7 @@ class SylvanBddManager {
     /**
      * Destroys Sylvan
      */
-    ~SylvanBddManager() {
-        sylvan::Sylvan::quitPackage();
-        lace_exit();
-    }
+    ~SylvanBddManager() = default;
 
     /**
      * Creates a variable with a unique name
@@ -180,6 +121,7 @@ class SylvanBddManager {
     }
 
    private:
+    storm::dd::InternalDdManager<storm::dd::DdType::Sylvan> internalManager{};
     uint32_t nextFreeVariableIndex{0};
 
     std::map<std::string, uint32_t> nameToIndex{};
