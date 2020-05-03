10253b25f2e55403b7e037bad944088f1fe52bb0
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2020-05-02 18:42:02 -0700

    removed xerces-c source from storm. If xerces-c is unavailable, storm will build everything as before, but storm-gspn will not be able to load gspns in XML format.

--- resources/3rdparty/include_xerces.cmake
+++ resources/3rdparty/include_xerces.cmake
@@ -2,47 +2,25 @@ if(USE_XERCESC)
 	find_package(XercesC QUIET)
     if(XercesC_FOUND)
         message(STATUS "Storm - Use system version of xerces.")
-    else()
-        message(STATUS "Storm - Use shipped version of xerces.")
-        set(XercesC_LIB_DIR ${STORM_3RDPARTY_BINARY_DIR}/xercesc-3.1.2/lib)
-        ExternalProject_Add(
-                xercesc
-                SOURCE_DIR ${STORM_3RDPARTY_SOURCE_DIR}/xercesc-3.1.2
-                CONFIGURE_COMMAND ${STORM_3RDPARTY_SOURCE_DIR}/xercesc-3.1.2/configure --prefix=${STORM_3RDPARTY_BINARY_DIR}/xercesc-3.1.2 --libdir=${XercesC_LIB_DIR} CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} CFLAGS=-O3 CXXFLAGS=-O3
-                PREFIX ${STORM_3RDPARTY_BINARY_DIR}/xercesc-3.1.2
-                BUILD_COMMAND make
-                BUILD_IN_SOURCE 0
-                LOG_CONFIGURE ON
-                LOG_BUILD ON
-                LOG_INSTALL ON
-                BUILD_BYPRODUCTS ${XercesC_LIB_DIR}/libxerces-c${DYNAMIC_EXT} ${XercesC_LIB_DIR}/libxerces-c${STATIC_EXT}
-        )
+        set(STORM_HAVE_XERCES ON)
+        include_directories(${XercesC_INCLUDE_DIRS})
+        if(APPLE)
+            FIND_LIBRARY(COREFOUNDATION_LIBRARY CoreFoundation )
+            FIND_LIBRARY(CORESERVICES_LIBRARY CoreServices )
+            mark_as_advanced(COREFOUNDATION_LIBRARY)
+            mark_as_advanced(CORESERVICES_LIBRARY)
+            string(REPLACE ".so" ".dylib" XercesC_LIBRARIES ${XercesC_LIBRARIES})
 
-        set(XercesC_ROOT ${STORM_3RDPARTY_BINARY_DIR}/xercesc-3.1.2)
-        set(XercesC_INCLUDE_DIRS ${XercesC_ROOT}/include)
-        set(XercesC_LIBRARY_PATH ${XercesC_LIB_DIR})
-
-        if(BUILD_STATIC)
-            set(XercesC_LIBRARIES ${XercesC_LIBRARY_PATH}/libxerces-c${STATIC_EXT})
-        else()
-            set(XercesC_LIBRARIES ${XercesC_LIBRARY_PATH}/libxerces-c${DYNAMIC_EXT})
         endif()
 
-        add_dependencies(resources xercesc)
-    endif()
 
-    message (STATUS "Storm - Linking with xercesc.")
-    set(STORM_HAVE_XERCES ON)
-    include_directories(${XercesC_INCLUDE_DIRS})
-    if(APPLE)
-        FIND_LIBRARY(COREFOUNDATION_LIBRARY CoreFoundation )
-        FIND_LIBRARY(CORESERVICES_LIBRARY CoreServices )
-		mark_as_advanced(COREFOUNDATION_LIBRARY)
-		mark_as_advanced(CORESERVICES_LIBRARY)
+        # find_package(CURL)
+        message (STATUS "Storm (GSPN) - Linking with xercesc: ${XercesC_LIBRARIES}")
+
+        list(APPEND STORM_GSPN_LINK_LIBRARIES ${XercesC_LIBRARIES} ${COREFOUNDATION_LIBRARY} ${CORESERVICES_LIBRARY} ${CURL_LIBRARIES})
+    else()
+        set(STORM_HAVE_XERCES OFF)
+        message (STATUS "Storm - Building without Xerces disables parsing XML formats (for GSPNs)")
     endif()
-    # find_package(CURL)
-    list(APPEND STORM_GSPN_LINK_LIBRARIES ${XercesC_LIBRARIES} ${COREFOUNDATION_LIBRARY} ${CORESERVICES_LIBRARY} ${CURL_LIBRARIES})
-else()
-    set(STORM_HAVE_XERCES OFF)
-    message (WARNING "Storm - Building without Xerces disables parsing XML formats (for GSPNs)")
 endif(USE_XERCESC)
+
