0378dca99840ce54ef59f4dccbcd7038dbcca7ad
shahid-khan <59087346+shahid-khan@users.noreply.github.com>
2020-08-21 12:53:18 +0200

    setting new brach

---
+++ src/storm-pgcl copy/storage/pgcl/ObserveStatement.cpp
@@ -0,0 +1,19 @@
+#include "ObserveStatement.h"
+#include "storm-pgcl/storage/pgcl/AbstractStatementVisitor.h"
+
+namespace storm {
+    namespace pgcl {
+        ObserveStatement::ObserveStatement(storm::pgcl::BooleanExpression const& condition) : condition(condition) {
+        }
+
+        void ObserveStatement::accept(storm::pgcl::AbstractStatementVisitor& visitor) {
+            visitor.visit(*this);
+        }
+
+        storm::pgcl::BooleanExpression const& ObserveStatement::getCondition() const {
+            return this->condition;
+        }
+
+    }
+}
+
