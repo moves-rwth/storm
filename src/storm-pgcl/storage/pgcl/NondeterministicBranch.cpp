cf8245082bacedeb43e6ae144ca433faed081190
shahid-khan <59087346+shahid-khan@users.noreply.github.com>
2020-08-24 09:34:40 +0200

    telecom exmaple added

---
+++ src/storm-pgcl/storage/pgcl/NondeterministicBranch.cpp
@@ -0,0 +1,20 @@
+#include "NondeterministicBranch.h"
+#include "storm-pgcl/storage/pgcl/AbstractStatementVisitor.h"
+
+namespace storm {
+    namespace pgcl {
+        NondeterministicBranch::NondeterministicBranch(std::shared_ptr<storm::pgcl::PgclBlock> const& left, std::shared_ptr<storm::pgcl::PgclBlock> const& right) {
+            leftBranch = left;
+            rightBranch = right;
+        }
+
+        void NondeterministicBranch::accept(storm::pgcl::AbstractStatementVisitor& visitor) {
+            visitor.visit(*this);
+        }
+
+        bool NondeterministicBranch::isNondet() const {
+            return true;
+        }
+    }
+}
+
