98a8263b31db2e82d04af358fffc63a0722f4005
Alexander Bork <alexander.bork@rwth-aachen.de>
2020-08-27 16:51:48 +0200

    Added support for reward computation

--- src/storm-pomdp/modelchecker/BeliefExplorationPomdpModelChecker.cpp
+++ src/storm-pomdp/modelchecker/BeliefExplorationPomdpModelChecker.cpp
@@ -748,7 +748,6 @@ namespace storm {
             template<typename PomdpModelType, typename BeliefValueType>
             bool BeliefExplorationPomdpModelChecker<PomdpModelType, BeliefValueType>::buildUnderApproximation(std::set<uint32_t> const &targetObservations, bool min, bool computeRewards, bool refine, HeuristicParameters const& heuristicParameters, std::shared_ptr<BeliefManagerType>& beliefManager, std::shared_ptr<ExplorerType>& underApproximation) {
                 bool useBeliefCulling = heuristicParameters.cullingThreshold > storm::utility::zero<ValueType>();
-                STORM_LOG_ASSERT(!computeRewards || !useBeliefCulling, "Reward Computation with Belief Culling is not implemented yet!");
                 STORM_LOG_INFO_COND(!useBeliefCulling, "Use Belief Culling with threshold " << storm::utility::to_string(heuristicParameters.cullingThreshold));
                 statistics.underApproximationBuildTime.start();
                 bool fixPoint = true;
@@ -811,7 +810,11 @@ namespace storm {
                             auto cullingResult = beliefManager->cullBelief(currId, heuristicParameters.cullingThreshold);
                             if(cullingResult.isCullable){
                                 bool addedSucc = underApproximation->addTransitionToBelief(0, cullingResult.targetBelief, storm::utility::one<ValueType>() - cullingResult.delta, false);
-                                underApproximation->addTransitionsToExtraStates(0, storm::utility::zero<ValueType>(), cullingResult.delta);
+                                if(computeRewards){
+                                    underApproximation->addTransitionsToExtraStates(0, cullingResult.delta);
+                                } else {
+                                    underApproximation->addTransitionsToExtraStates(0, storm::utility::zero<ValueType>(), cullingResult.delta);
+                                }
                                 STORM_LOG_ASSERT(addedSucc, "Didn't add a transition after culling.");
                             } else {
                                 stopExploration = true;
