812339a5ebbad5d952ac6424ae24a8fe623a5db4
Johannes Lehmann <johannesalehmann@gmail.com>
2021-01-15 00:20:47 +0100

    Refactored JaniLocalEliminator tests

--- src/test/storm/storage/JaniLocalEliminatorTests.cpp
+++ src/test/storm/storage/JaniLocalEliminatorTests.cpp
@@ -10,6 +10,8 @@
 #include "storm/storage/jani/JaniLocalEliminator.h"
 #include "storm/storage/jani/JSONExporter.h"
 #include "storm/storage/expressions/ExpressionManager.h"
+#include "storm/storage/expressions/Expression.h"
+#include "storm/storage/expressions/Variable.h"
 #include <storm/storage/jani/Property.h>
 #include <storm/modelchecker/results/CheckResult.h>
 #include <storm/modelchecker/results/ExplicitQuantitativeCheckResult.h>
@@ -24,11 +26,32 @@ typedef storm::modelchecker::SparseDtmcPrctlModelChecker<Dtmc> DtmcModelChecker;
 using storm::jani::JaniLocalEliminator;
 
 std::pair<storm::jani::Model, std::vector<storm::jani::Property>> saveAndReload(storm::jani::Model model, std::vector<storm::jani::Property> props){
-
-// storm::jani::Model saveAndReload(storm::jani::Model model, std::vector<storm::jani::Property> props){
     storm::jani::JsonExporter::toFile(model, props, STORM_TEST_RESOURCES_DIR "/dtmc/_temp_model.jani", true, false);
     auto minModelProps = storm::api::parseJaniModel(STORM_TEST_RESOURCES_DIR "/dtmc/_temp_model.jani", storm::jani::getAllKnownModelFeatures(), boost::none);
     return minModelProps;
+}
+
+void checkModel(storm::jani::Model model, std::vector<storm::jani::Property> properties, std::map<storm::expressions::Variable, storm::expressions::Expression> consts, int states, int transitions, double expectedValue){
+    model = model.defineUndefinedConstants(consts);
+    properties[0] = properties[0].substitute(consts);
+
+    auto formulae = storm::api::extractFormulasFromProperties(properties);
+    storm::builder::BuilderOptions  options(formulae, model);
+    options.setBuildAllLabels(true);
+    auto explicitModel = storm::api::buildSparseModel<double>(model, options)->template as<Dtmc>();
+
+    EXPECT_EQ(states, explicitModel->getNumberOfStates());
+    EXPECT_EQ(transitions, explicitModel->getNumberOfTransitions());
+
+    auto task = storm::modelchecker::CheckTask<>(*(formulae[0]), true);
+    storm::Environment env;
+    auto checkResult = storm::api::verifyWithSparseEngine<double>(env, explicitModel, task);
+    auto quantResult = checkResult->asExplicitQuantitativeCheckResult<double>();
+
+    auto initialStates = explicitModel->getInitialStates();
+    for (auto state = initialStates.begin(); state != initialStates.end(); ++state){
+        EXPECT_NEAR(expectedValue, quantResult[*state], storm::settings::getModule<storm::settings::modules::GeneralSettings>().getPrecision());
+    }
 }
 
 TEST(JaniLocalEliminator, ExampleTest) {
@@ -45,32 +68,10 @@ TEST(JaniLocalEliminator, ExampleTest) {
     auto modelMin = minModelProps.first;
     auto propsMin = minModelProps.second;
 
+    std::map<storm::expressions::Variable, storm::expressions::Expression> consts = {
+            {modelMin.getConstant("N").getExpressionVariable(), modelMin.getExpressionManager().integer(20)},
+            {modelMin.getConstant("K").getExpressionVariable(), modelMin.getExpressionManager().integer(1)}
+    };
 
-    modelMin = modelMin.defineUndefinedConstants({
-        {modelMin.getConstant("N").getExpressionVariable(), modelMin.getExpressionManager().integer(20)},
-        {modelMin.getConstant("K").getExpressionVariable(), modelMin.getExpressionManager().integer(1)}
-    });
-    propsMin[0] = propsMin[0].substitute({
-        {modelMin.getConstant("N").getExpressionVariable(), modelMin.getExpressionManager().integer(20)},
-        {modelMin.getConstant("K").getExpressionVariable(), modelMin.getExpressionManager().integer(1)}
-    });
-
-    auto formulae = storm::api::extractFormulasFromProperties(propsMin);
-    storm::builder::BuilderOptions  options(formulae, modelMin);
-    options.setBuildAllLabels(true);
-
-    auto explicitModel = storm::api::buildSparseModel<double>(modelMin, options)->template as<Dtmc>();
-
-    EXPECT_EQ(16182, explicitModel->getNumberOfStates());
-    EXPECT_EQ(58102, explicitModel->getNumberOfTransitions());
-
-    auto task = storm::modelchecker::CheckTask<>(*(formulae[0]), true);
-    storm::Environment env;
-    auto checkResult = storm::api::verifyWithSparseEngine<double>(env, explicitModel, task);
-    auto quantResult = checkResult->asExplicitQuantitativeCheckResult<double>();
-
-    auto initialStates = explicitModel->getInitialStates();
-    for (auto state = initialStates.begin(); state != initialStates.end(); ++state){
-        EXPECT_NEAR(0.28641904638485044, quantResult[*state], storm::settings::getModule<storm::settings::modules::GeneralSettings>().getPrecision());
-    }
+    checkModel(modelMin, propsMin, consts, 16182, 58102, 0.28641904638485044);
 }
