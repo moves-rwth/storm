e57f06f828f261561f4f1f8c2d16b4dc155540a0
Linus <justwitti@gmail.com>
2021-10-06 16:57:43 +0200

    Derivative merge (#111)
    * manual rebase: Add gradient descent on top of master
    
    * Remove compile_commands
    
    * Fix division by zero in ADAM and tests
    
    * WIP: Fix non-fixpoint solvers
    
    * WIP: Adam functionality test
    
    * Fix some details
    
    * Fix sign...
    
    * Fix inconsequential parameters
    
    * Add start-point option
    
    * Add adam
    
    * Update .gitignore
    
    Co-authored-by: Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
    
    * Add a print to the DerivativeEvaluationHelperTest to help me understand why it fails on travis CI
    
    * WIP: Fix test, I think it was a problem with the variable pool. Let's try travis once more
    
    * Rewrite tests as cool TYPED_TESTS, do not store Environments inside of the GradientDescentInstantiationSearcher and DerivativeEvaluationHelper but take them as first argument
    
    * Fix test compilation on gcc
    
    * fix nesterov?, radam
    
    * fix nesterov?
    
    * Fix nesterov
    
    * min() takes the same type
    
    * stepNum -> ConstantType
    
    * Rename EvaluationHelper to ModelChecker
    
    * Fix reward models
    
    * Initial try for projecting bounds
    
    * Add no-project-gradient option
    
    * Logistic function
    
    * Implement command line for choosing projection method
    
    * More barrier experiments
    
    * Log barrier implementation, I guess
    
    * Fix nesterov in combination with logistic sigmoid
    
    * fix projected gradient
    
    Co-authored-by: Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>

---
+++ resources/examples/testfiles/pdtmc/gradient2.pm
@@ -0,0 +1,17 @@
+dtmc
+
+const double p;
+const double q;
+
+module test
+
+	// local state
+	s : [0..4] init 0;
+
+	
+	[] s=0 -> p : (s'=1) + (1-p) : (s'=3);
+	[] s=1 -> q : (s'=3) + (1-q) : (s'=2);
+	[] s=2 -> 1 : (s'=2);
+	[] s=3 -> 1 : (s'=3);
+endmodule
+
