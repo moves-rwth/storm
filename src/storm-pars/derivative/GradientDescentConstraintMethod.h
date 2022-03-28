6ad6b4aaa1eb93754f84474a9d867be7062a3097
Linus Heck <linus.heck@rwth-aachen.de>
2022-03-28 18:28:31 +0200

    Fix Gradient Descent bugs

--- src/storm-pars/derivative/GradientDescentConstraintMethod.h
+++ src/storm-pars/derivative/GradientDescentConstraintMethod.h
@@ -3,19 +3,19 @@
 #include <boost/optional.hpp>
 #include <string>
 namespace storm {
-	namespace derivative {
-		/**
-		 * GradientDescentConstraintMethod is the method for mitigating constraints
-		 * that the GradientDescentInstantiationSearcher uses.
-		 */
-		enum class GradientDescentConstraintMethod {
-			PROJECT_WITH_GRADIENT, //< The default.
-			PROJECT,
-			PENALTY_QUADRATIC,
-			BARRIER_LOGARITHMIC,
-			BARRIER_INFINITY,
-			LOGISTIC_SIGMOID
-		};
-	}
-}
+namespace derivative {
+/**
+ * GradientDescentConstraintMethod is the method for mitigating constraints
+ * that the GradientDescentInstantiationSearcher uses.
+ */
+enum class GradientDescentConstraintMethod {
+    PROJECT_WITH_GRADIENT,  //< The default.
+    PROJECT,
+    PENALTY_QUADRATIC,
+    BARRIER_LOGARITHMIC,
+    BARRIER_INFINITY,
+    LOGISTIC_SIGMOID
+};
+}  // namespace derivative
+}  // namespace storm
 #endif
