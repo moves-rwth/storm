7159c39d60bbb85a4c9422477c1dd18358713e2a
Alex Bork <alexander.bork@cs.rwth-aachen.de>
2022-06-08 17:50:10 +0200

    Testing configuration

--- src/storm-pomdp/modelchecker/BeliefExplorationPomdpModelChecker.cpp
+++ src/storm-pomdp/modelchecker/BeliefExplorationPomdpModelChecker.cpp
@@ -900,7 +900,7 @@ namespace storm {
                             }
                         }  // end Clipping Procedure
 
-                        if(!stopExploration || useExtendedCutoff) {
+                        //if(!stopExploration || useExtendedCutoff) {
                             // Add successor transitions or cut-off transitions when exploration is stopped
                             for (uint64_t action = 0, numActions = beliefManager->getBeliefNumberOfChoices(currId); action < numActions; ++action) {
                                 // Always restore old behavior if available
@@ -917,7 +917,9 @@ namespace storm {
                                             STORM_LOG_ASSERT(stopExploration, "Didn't add a transition although exploration shouldn't be stopped.");
                                             // We did not explore this successor state. Get a bound on the "missing" value
                                             truncationProbability += successor.second;
-                                            // Some care has to be taken here: Essentially, we are triangulating a value for the under-approximation out of other under-approximation values. In general, this does not yield a sound underapproximation anymore. However, in our case this is still the case as the under-approximation values are based on a memoryless scheduler.
+                                            // Some care has to be taken here: Essentially, we are triangulating a value for the under-approximation out of other under-approximation values.
+                                            // In general, this does not yield a sound underapproximation anymore.
+                                            // However, in our case this is still the case as the under-approximation values are based on a memoryless scheduler.
                                             truncationValueBound +=
                                                 successor.second * (min ? underApproximation->computeUpperValueBoundAtBelief(successor.first)
                                                                         : underApproximation->computeLowerValueBoundAtBelief(successor.first));
@@ -942,7 +944,7 @@ namespace storm {
                                     }
                                 }
                             }
-                        } else {
+                        /*} else {
                             // Add one cut-off transition for each cut-off strategy
                             for (uint64_t i = 0; i < nrCutoffStrategies; ++i) {
                                 auto cutOffValue = min ? underApproximation->computeUpperValueBoundForScheduler(currId, i) : underApproximation->computeLowerValueBoundForScheduler(currId, i) ;
@@ -953,7 +955,7 @@ namespace storm {
                                     underApproximation->addTransitionsToExtraStates(i, cutOffValue,storm::utility::one<ValueType>() - cutOffValue);
                                 }
                             }
-                        }
+                        }*/
                     }
                     if (storm::utility::resources::isTerminate()) {
                         break;
