7b3bc0460c683d65cab5758be07c54fc9df8cfd1
Matthias Volk <m.volk@utwente.nl>
2022-09-14 14:36:56 +0200

    Use EXPECT_NEAR in tests for storm-dft

--- src/test/storm-dft/api/DftModelCheckerTest.cpp
+++ src/test/storm-dft/api/DftModelCheckerTest.cpp
@@ -102,6 +102,14 @@ class DftModelCheckerTest : public ::testing::Test {
         return analyze(file, property);
     }
 
+    double precision() {
+        return 1e-12;
+    }
+
+    double precisionReliability() {
+        return 1e-10;
+    }
+
    private:
     DftAnalysisConfig config;
 };
@@ -112,23 +120,23 @@ TYPED_TEST_SUITE(DftModelCheckerTest, TestingTypes, );
 
 TYPED_TEST(DftModelCheckerTest, AndMTTF) {
     double result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/and.dft");
-    EXPECT_FLOAT_EQ(result, 3);
+    EXPECT_NEAR(result, 3, this->precision());
 }
 
 TYPED_TEST(DftModelCheckerTest, OrMTTF) {
     double result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/or.dft");
-    EXPECT_FLOAT_EQ(result, 1);
+    EXPECT_NEAR(result, 1, this->precision());
 }
 
 TYPED_TEST(DftModelCheckerTest, VotingMTTF) {
     double result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/voting.dft");
-    EXPECT_FLOAT_EQ(result, 5 / 3.0);
+    EXPECT_NEAR(result, 5 / 3.0, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/voting2.dft");
-    EXPECT_FLOAT_EQ(result, 10 / 17.0);
+    EXPECT_NEAR(result, 10 / 17.0, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/voting3.dft");
-    EXPECT_FLOAT_EQ(result, 2685 / 1547.0);
+    EXPECT_NEAR(result, 2685 / 1547.0, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/voting4.dft");
-    EXPECT_FLOAT_EQ(result, 5 / 6.0);
+    EXPECT_NEAR(result, 5 / 6.0, this->precision());
 }
 
 TYPED_TEST(DftModelCheckerTest, PandMTTF) {
@@ -143,9 +151,9 @@ TYPED_TEST(DftModelCheckerTest, PorMTTF) {
 
 TYPED_TEST(DftModelCheckerTest, FdepMTTF) {
     double result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/fdep2.dft");
-    EXPECT_FLOAT_EQ(result, 2);
+    EXPECT_NEAR(result, 2, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/fdep3.dft");
-    EXPECT_FLOAT_EQ(result, 5 / 2.0);
+    EXPECT_NEAR(result, 5 / 2.0, this->precision());
 
     if (this->getConfig().useMod) {
         STORM_SILENT_EXPECT_THROW(this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/fdep.dft"), storm::exceptions::NotSupportedException);
@@ -153,25 +161,25 @@ TYPED_TEST(DftModelCheckerTest, FdepMTTF) {
         STORM_SILENT_EXPECT_THROW(this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/fdep5.dft"), storm::exceptions::NotSupportedException);
     } else {
         double result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/fdep.dft");
-        EXPECT_FLOAT_EQ(result, 2 / 3.0);
+        EXPECT_NEAR(result, 2 / 3.0, this->precision());
         result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/fdep4.dft");
-        EXPECT_FLOAT_EQ(result, 1);
+        EXPECT_NEAR(result, 1, this->precision());
         result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/fdep5.dft");
-        EXPECT_FLOAT_EQ(result, 3);
+        EXPECT_NEAR(result, 3, this->precision());
     }
 }
 
 TYPED_TEST(DftModelCheckerTest, PdepMTTF) {
     double result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/pdep.dft");
-    EXPECT_FLOAT_EQ(result, 8 / 3.0);
+    EXPECT_NEAR(result, 8 / 3.0, this->precision());
     if (this->getConfig().useMod && !this->getConfig().useDC) {
         STORM_SILENT_EXPECT_THROW(this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/pdep2.dft"), storm::exceptions::NotSupportedException);
     } else {
         result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/pdep2.dft");
-        EXPECT_FLOAT_EQ(result, 38 / 15.0);
+        EXPECT_NEAR(result, 38 / 15.0, this->precision());
     }
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/pdep3.dft");
-    EXPECT_FLOAT_EQ(result, 67 / 24.0);
+    EXPECT_NEAR(result, 67 / 24.0, this->precision());
 
     if (this->getConfig().useMod) {
         STORM_SILENT_EXPECT_THROW(this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/pdep4.dft"), storm::exceptions::NotSupportedException);
@@ -183,52 +191,52 @@ TYPED_TEST(DftModelCheckerTest, PdepMTTF) {
 
 TYPED_TEST(DftModelCheckerTest, SpareMTTF) {
     double result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/spare.dft");
-    EXPECT_FLOAT_EQ(result, 46 / 13.0);
+    EXPECT_NEAR(result, 46 / 13.0, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/spare2.dft");
-    EXPECT_FLOAT_EQ(result, 43 / 23.0);
+    EXPECT_NEAR(result, 43 / 23.0, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/spare3.dft");
-    EXPECT_FLOAT_EQ(result, 14 / 11.0);
+    EXPECT_NEAR(result, 14 / 11.0, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/spare4.dft");
-    EXPECT_FLOAT_EQ(result, 18836 / 3887.0);
+    EXPECT_NEAR(result, 18836 / 3887.0, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/spare5.dft");
-    EXPECT_FLOAT_EQ(result, 8 / 3.0);
+    EXPECT_NEAR(result, 8 / 3.0, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/spare6.dft");
-    EXPECT_FLOAT_EQ(result, 7 / 5.0);
+    EXPECT_NEAR(result, 7 / 5.0, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/spare7.dft");
-    EXPECT_FLOAT_EQ(result, 551 / 150.0);
+    EXPECT_NEAR(result, 551 / 150.0, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/spare8.dft");
-    EXPECT_FLOAT_EQ(result, 249 / 52.0);  // DFTCalc has result of 4.33779 due to different semantics of nested spares
+    EXPECT_NEAR(result, 249 / 52.0, this->precision());  // DFTCalc has result of 4.33779 due to different semantics of nested spares
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/spare_dc.dft");
-    EXPECT_FLOAT_EQ(result, 78311 / 182700.0);
+    EXPECT_NEAR(result, 78311 / 182700.0, this->precision());
 }
 
 TYPED_TEST(DftModelCheckerTest, SeqMTTF) {
     double result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/seq.dft");
-    EXPECT_FLOAT_EQ(result, 4);
+    EXPECT_NEAR(result, 4, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/seq2.dft");
-    EXPECT_FLOAT_EQ(result, 6);
+    EXPECT_NEAR(result, 6, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/seq3.dft");
-    EXPECT_FLOAT_EQ(result, 6);
+    EXPECT_NEAR(result, 6, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/seq4.dft");
-    EXPECT_FLOAT_EQ(result, 6);
+    EXPECT_NEAR(result, 6, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/seq5.dft");
     EXPECT_EQ(result, storm::utility::infinity<double>());
 
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/mutex.dft");
-    EXPECT_FLOAT_EQ(result, 1 / 2.0);
+    EXPECT_NEAR(result, 1 / 2.0, this->precision());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/mutex2.dft");
-    EXPECT_FLOAT_EQ(result, storm::utility::infinity<double>());
+    EXPECT_EQ(result, storm::utility::infinity<double>());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/mutex3.dft");
-    EXPECT_FLOAT_EQ(result, storm::utility::infinity<double>());
+    EXPECT_EQ(result, storm::utility::infinity<double>());
     result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/seq6.dft");
-    EXPECT_FLOAT_EQ(result, 30000);
+    EXPECT_NEAR(result, 30000, this->precision());
 }
 
 TYPED_TEST(DftModelCheckerTest, Symmetry) {
     double result = this->analyzeMTTF(STORM_TEST_RESOURCES_DIR "/dft/symmetry6.dft");
-    EXPECT_FLOAT_EQ(result, 2804183 / 2042040.0);
+    EXPECT_NEAR(result, 2804183 / 2042040.0, this->precision());
     result = this->analyzeReliability(STORM_TEST_RESOURCES_DIR "/dft/symmetry6.dft", 1.0);
-    EXPECT_FLOAT_EQ(result, 0.3421934224);
+    EXPECT_NEAR(result, 0.3421934224, this->precisionReliability());
 }
 
 TYPED_TEST(DftModelCheckerTest, HecsReliability) {
@@ -238,6 +246,6 @@ TYPED_TEST(DftModelCheckerTest, HecsReliability) {
         return;
     }
     double result = this->analyzeReliability(STORM_TEST_RESOURCES_DIR "/dft/hecs_2_2.dft", 1.0);
-    EXPECT_FLOAT_EQ(result, 0.00021997582);
+    EXPECT_NEAR(result, 0.00021997582, this->precisionReliability());
 }
 }  // namespace
