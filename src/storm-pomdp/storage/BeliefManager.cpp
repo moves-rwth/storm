e4fe7abf12c5e3ed3336ef456a0b0eeb2da22321
Filip Macak <filip.macak1999@gmail.com>
2022-09-18 21:01:31 +0200

    make missing label warning a trace to not clutter Storm output

--- src/storm-pomdp/storage/BeliefManager.cpp
+++ src/storm-pomdp/storage/BeliefManager.cpp
@@ -956,7 +956,7 @@ namespace storm {
             if(pomdp.hasObservationValuations()){
                 return pomdp.getObservationValuations().getStateInfo(getBeliefObservation(beliefId));
             } else {
-                STORM_LOG_WARN("Cannot get observation labels as no observation valuation has been defined for the POMDP. Return empty label instead.");
+                STORM_LOG_TRACE("Cannot get observation labels as no observation valuation has been defined for the POMDP. Return empty label instead.");
                 return "";
             }
 
