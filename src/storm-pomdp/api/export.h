13447e8718d56e01c46e4c62c582247fdd2b3c9c
Alex Bork <alexander.bork@cs.rwth-aachen.de>
2022-10-07 12:23:59 +0200

    Julia export CLI handling

--- src/storm-pomdp/api/export.h
+++ src/storm-pomdp/api/export.h
@@ -1,8 +1,18 @@
-#include "storm-pomdp/modelchecker/BeliefExplorationPomdpModelChecker.h"
+#pragma once
+
+#include "storm-pomdp/io/PomdpToJuliaExport.h"
+#include "storm/io/file.h"
+
 namespace storm {
 namespace pomdp {
 namespace api {
 template<typename ValueType>
+void exportSparsePomdpAsJuliaModel(std::shared_ptr<storm::models::sparse::Pomdp<ValueType>> const& model, std::string const& filename, double discount, storm::logic::Formula const& formula) {
+    std::ofstream stream;
+    storm::utility::openFile(filename, stream);
+    storm::pomdp::exporter::exportPomdpToJulia(stream,model,  discount, formula);
+    storm::utility::closeFile(stream);
+}
 
 }
 }
