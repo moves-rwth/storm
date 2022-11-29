725e01dbce1ad1367f294f0c1adbc979202b9fda
Jip Spel <jip.spel@cs.rwth-aachen.de>
2022-11-29 17:37:57 +0100

    Nu moet ie kloppen

--- src/storm-pars/modelchecker/region/SparseParameterLiftingModelChecker.cpp
+++ src/storm-pars/modelchecker/region/SparseParameterLiftingModelChecker.cpp
@@ -482,7 +482,11 @@ SparseParameterLiftingModelChecker<SparseModelType, ConstantType>::computeExtrem
                         }
 
                         // We set the bounds to extend the order for the new regions.
-                        if (useMonotonicity && this->isUseBoundsSet() && !order->getDoneBuilding() && orderExtender) {
+                        if (useMonotonicity && this->isUseBoundsSet() && !order->getDoneBuilding() && orderExtender &&
+                            (storm::utility::convertNumber<double>(order->getNumberOfSufficientStates()) /
+                                     storm::utility::convertNumber<double>(order->getNumberOfStates()) <
+                                 0.25 ||
+                             this->isDisableOptimizationSet())) {
                             numberOfPLACallsBounds++;
                             if (minimize) {
                                 orderExtender->setMinMaxValues(
