aed0ca7d0977f92f00e952dd12ccb13da0f34f62
Alex Bork <alexander.bork@cs.rwth-aachen.de>
2022-12-13 14:05:42 +0100

    Added interface and test for skipping cut-offs using heuristic schedulers

--- src/storm-pomdp/api/verification.h
+++ src/storm-pomdp/api/verification.h
@@ -16,6 +16,19 @@ namespace api {
     }
 
     template<typename ValueType>
+    typename storm::pomdp::modelchecker::BeliefExplorationPomdpModelChecker<storm::models::sparse::Pomdp<ValueType>>::Result
+    underapproximateWithoutHeuristicValues(storm::Environment const& env, std::shared_ptr<storm::models::sparse::Pomdp<ValueType>> pomdp,
+                                storm::modelchecker::CheckTask<storm::logic::Formula, ValueType> const& task, uint64_t sizeThreshold,  std::vector<std::vector<std::unordered_map<uint64_t,ValueType>>> pomdpStateValues){
+        storm::pomdp::modelchecker::BeliefExplorationPomdpModelCheckerOptions<ValueType> options(false,true);
+        options.skipHeuristicSchedulers = true;
+        options.useGridClipping = false;
+        options.useExplicitCutoff = true;
+        options.sizeThresholdInit = sizeThreshold;
+        storm::pomdp::modelchecker::BeliefExplorationPomdpModelChecker<storm::models::sparse::Pomdp<ValueType>> modelchecker(pomdp, options);
+        return modelchecker.check(task.getFormula(), pomdpStateValues);
+    }
+
+    template<typename ValueType>
     std::shared_ptr<storm::models::sparse::Model<ValueType>> extractSchedulerAsMarkovChain(typename storm::pomdp::modelchecker::BeliefExplorationPomdpModelChecker<storm::models::sparse::Pomdp<ValueType>>::Result modelcheckingResult){
         return modelcheckingResult.schedulerAsMarkovChain;
     }
