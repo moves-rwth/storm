04ed0a87b0a7eb4eac83301f6b19009ba9365c9c
Jip Spel <jip.spel@cs.rwth-aachen.de>
2023-03-13 10:59:26 +0100

    Remove test folder

--- test/src/storm-gspn/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jipspel/Documents/Tools/storm/src/storm-gspn
-# Build directory: /Users/jipspel/Documents/Tools/storm/test/src/storm-gspn
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
