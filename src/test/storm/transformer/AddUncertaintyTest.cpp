72b1fc3a6515e34c465678e495ed6886fd5c6925
Sebastian Junges <sebastian.junges@gmail.com>
2023-12-19 11:48:59 +0100

    Update src/test/storm/transformer/AddUncertaintyTest.cpp
    Co-authored-by: Matthias Volk <volkm@users.noreply.github.com>

--- src/test/storm/transformer/AddUncertaintyTest.cpp
+++ src/test/storm/transformer/AddUncertaintyTest.cpp
@@ -28,4 +28,5 @@ TEST(AddUncertaintyTransformerTest, Coin22Test) {
     auto uncertainModel = transformer.transform(0.01);
     EXPECT_EQ(uncertainModel->getNumberOfStates(), model->getNumberOfStates());
     EXPECT_EQ(uncertainModel->getNumberOfTransitions(), model->getNumberOfTransitions());
+    EXPECT_TRUE(uncertainModel->hasUncertainty());
 }
