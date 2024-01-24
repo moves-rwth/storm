6f2049997eba9dcd8aa7e9f529985f7798aa2fc4
Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
2024-01-24 11:13:58 +0100

    Improved expected visiting times (EVTs) and steady state distribution computations (#493)
    --sound mode now works properly with correct accuracy using OVI and II for EVTs.
    Steady-state distributions can now be computed using a reduction to EVT-based computations.
    
    Using --expvisittimes --prop 'Phi' now restricts the result to states satisfying qualitative formula Phi.
    --steadystate --prop 'Phi' works similarly.
    
    Co-authored-by: m-hannah <hannah.mertens@rwth-aachen.de>

--- src/storm/settings/modules/IOSettings.h
+++ src/storm/settings/modules/IOSettings.h
@@ -6,6 +6,7 @@
 #include "storm-config.h"
 #include "storm/builder/ExplorationOrder.h"
 #include "storm/io/ModelExportFormat.h"
+#include "storm/modelchecker/helper/infinitehorizon/SteadyStateDistributionAlgorithm.h"
 #include "storm/settings/modules/ModuleSettings.h"
 
 namespace storm {
@@ -332,6 +333,11 @@ class IOSettings : public ModuleSettings {
      * Retrieves whether the steady-state distribution is to be computed.
      */
     bool isComputeSteadyStateDistributionSet() const;
+
+    /*!
+     * @return the algorithm used for computing steady state distributions
+     */
+    storm::SteadyStateDistributionAlgorithm getSteadyStateDistributionAlgorithm() const;
 
     /*!
      * Retrieves whether the expected visiting times are to be computed.
