e5a8519fb8da5738267f495fe254a2ea908536cb
Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
2024-02-13 22:29:48 +0100

    Fixed formatting

--- src/storm/modelchecker/helper/indefinitehorizon/visitingtimes/SparseDeterministicVisitingTimesHelper.cpp
+++ src/storm/modelchecker/helper/indefinitehorizon/visitingtimes/SparseDeterministicVisitingTimesHelper.cpp
@@ -296,7 +296,7 @@ storm::Environment SparseDeterministicVisitingTimesHelper<ValueType>::getEnviron
     auto prec = newEnv.solver().getPrecisionOfLinearEquationSolver(newEnv.solver().getLinearEquationSolverType());
     if (prec.first.is_initialized()) {
         STORM_LOG_INFO("Precision for EVTs computation: " << storm::utility::convertNumber<double>(prec.first.get()) << " (exact: " << prec.first.get() << ")"
-                                                       << '\n');
+                                                          << '\n');
     }
 
     return newEnv;
