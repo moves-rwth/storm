moves-rwth/storm

commit 8163e3c5e698fdac9b1ac8ed461d1b51ed9fc832

Fixes for explicit jani model builder (#507)
- Fixed building JANI models that use the same automaton multiple times in their system composition. Previously, the different instances referred to the same expression variables for the automaton location and local variables.

- Fixed building JaniChoiceOrigins which previously referred to internal automata indices and not the ones from the JANI model.

src/storm/generator/JaniNextStateGenerator.cpp
@@ -68,6 +68,8 @@ JaniNextStateGenerator<ValueType, StateType>::JaniNextStateGenerator(storm::jani
     }
     STORM_LOG_THROW(features.empty(), storm::exceptions::NotSupportedException,
                     "The explicit next-state generator does not support the following model feature(s): " << features.toString() << ".");
+    // Simplify the system compositions so that we can exclude the case where automata appear in the composition multiple times.
+    this->model.simplifyComposition();
 
     // Get the reward expressions to be build. Also find out whether there is a non-trivial one.
     bool hasNonTrivialRewardExpressions = false;
@@ -852,7 +854,8 @@ void JaniNextStateGenerator<ValueType, StateType>::generateSynchronizedDistribut
     uint64_t numDestinations = 1;
     for (uint_fast64_t i = 0; i < iteratorList.size(); ++i) {
         if (this->getOptions().isBuildChoiceOriginsSet()) {
-            edgeIndices.insert(model.encodeAutomatonAndEdgeIndices(edgeCombination[i].first, iteratorList[i]->first));
+            auto automatonIndex = model.getAutomatonIndex(parallelAutomata[edgeCombination[i].first].get().getName());
+            edgeIndices.insert(model.encodeAutomatonAndEdgeIndices(automatonIndex, iteratorList[i]->first));
         }
         storm::jani::Edge const& edge = *iteratorList[i]->second;
         lowestDestinationAssignmentLevel = std::min(lowestDestinationAssignmentLevel, edge.getLowestAssignmentLevel());
@@ -1069,6 +1072,7 @@ std::vector<Choice<ValueType>> JaniNextStateGenerator<ValueType, StateType>::get
                                                                 automatonIndex, state, stateToIdCallback));
 
                     if (this->getOptions().isBuildChoiceOriginsSet()) {
+                        auto modelAutomatonIndex = model.getAutomatonIndex(parallelAutomata[automatonIndex].get().getName());
                         EdgeIndexSet edgeIndex{model.encodeAutomatonAndEdgeIndices(automatonIndex, indexAndEdge.first)};
                         result.back().addOriginData(boost::any(std::move(edgeIndex)));
                     }

src/storm/storage/jani/Model.cpp
@@ -1008,9 +1008,11 @@ void Model::simplifyComposition() {
         }
     }
 
-    // Traverse the system composition and exchange automata by their copy
-    auto newComposition = CompositionSimplificationVisitor(automatonToCopiesMap).simplify(getSystemComposition());
-    this->setSystemComposition(newComposition);
+    if (!automatonToCopiesMap.empty()) {
+        // Traverse the system composition and exchange automata by their copy
+        auto newComposition = CompositionSimplificationVisitor(automatonToCopiesMap).simplify(getSystemComposition());
+        this->setSystemComposition(newComposition);
+    }
 }
 
 void Model::setSystemComposition(std::shared_ptr<Composition> const& composition) {

commit e29d4f91df7c1917fc068d1efa7a4428ad00ad9e

Jani builder: Fix referring to the wrong kind of automaton index
This fixes a small error introduced in PR #507

src/storm/generator/JaniNextStateGenerator.cpp
@@ -1073,7 +1073,7 @@ std::vector<Choice<ValueType>> JaniNextStateGenerator<ValueType, StateType>::get
 
                     if (this->getOptions().isBuildChoiceOriginsSet()) {
                         auto modelAutomatonIndex = model.getAutomatonIndex(parallelAutomata[automatonIndex].get().getName());
-                        EdgeIndexSet edgeIndex{model.encodeAutomatonAndEdgeIndices(automatonIndex, indexAndEdge.first)};
+                        EdgeIndexSet edgeIndex{model.encodeAutomatonAndEdgeIndices(modelAutomatonIndex, indexAndEdge.first)};
                         result.back().addOriginData(boost::any(std::move(edgeIndex)));
                     }
                 }