af80b7dd261b0f6bc34dabd060efe75edfb2c6fa
Sebastian Junges <sebastian.junges@ru.nl>
2024-05-21 23:09:17 +0200

    another minor refactoring

--- src/storm/logic/Bound.cpp
+++ src/storm/logic/Bound.cpp
@@ -1,5 +1,5 @@
 #include "storm/logic/Bound.h"
-#include "storm/adapters/RationalNumberAdapter.h"
+#include "storm/adapters/RationalFunctionAdapter.h"
 
 namespace storm::logic {
 
@@ -32,5 +32,6 @@ template bool Bound::isSatisfied(double const& compareValue) const;
 template bool Bound::isSatisfied(storm::RationalNumber const& compareValue) const;
 template double Bound::evaluateThresholdAs() const;
 template storm::RationalNumber Bound::evaluateThresholdAs() const;
+template storm::RationalFunction Bound::evaluateThresholdAs() const;
 
 }  // namespace storm::logic
