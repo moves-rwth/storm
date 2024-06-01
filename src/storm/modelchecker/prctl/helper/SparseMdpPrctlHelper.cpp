859167dee6dab70b669cff87087667c0f9eb0de9
Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
2024-06-01 22:46:25 +0200

    Added output of MEC statistics. (#558)
    When computing reachability probabilities or expected total reachability rewards, the MEC quotient of the "maybestates"-subsystem might be computed (either because it is required by the used algorithm or because the user has set the `--force-require-unique`.
    If this is the case and the `--verbose` has been set, some interesting statistics on the MECs will now be printed.

--- src/storm/modelchecker/prctl/helper/SparseMdpPrctlHelper.cpp
+++ src/storm/modelchecker/prctl/helper/SparseMdpPrctlHelper.cpp
@@ -662,6 +662,7 @@ boost::optional<SparseMdpEndComponentInformation<ValueType>> computeFixedPointSy
     if (doDecomposition) {
         // Compute the states that are in MECs.
         endComponentDecomposition = storm::storage::MaximalEndComponentDecomposition<ValueType>(transitionMatrix, backwardTransitions, candidateStates);
+        STORM_LOG_INFO(endComponentDecomposition.statistics(transitionMatrix.getRowGroupCount()));
     }
 
     // Only do more work if there are actually end-components.
@@ -1253,6 +1254,7 @@ boost::optional<SparseMdpEndComponentInformation<ValueType>> computeFixedPointSy
         // Then compute the states that are in MECs with zero reward.
         endComponentDecomposition =
             storm::storage::MaximalEndComponentDecomposition<ValueType>(transitionMatrix, backwardTransitions, candidateStates, zeroRewardChoices);
+        STORM_LOG_INFO(endComponentDecomposition.statistics(transitionMatrix.getRowGroupCount()));
     }
 
     // Only do more work if there are actually end-components.
