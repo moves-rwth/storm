8244cb3ee98b0e324fe8aff233b72e4256a9f08b
Filip Macak <filip.macak1999@gmail.com>
2024-09-24 14:12:04 +0200

    Made the code style consistent with the rest of the file

--- src/storm-pomdp/modelchecker/BeliefExplorationPomdpModelChecker.cpp
+++ src/storm-pomdp/modelchecker/BeliefExplorationPomdpModelChecker.cpp
@@ -1460,7 +1460,7 @@ BeliefExplorationPomdpModelChecker<PomdpModelType, BeliefValueType, BeliefMDPTyp
 template<typename PomdpModelType, typename BeliefValueType, typename BeliefMDPType>
 void BeliefExplorationPomdpModelChecker<PomdpModelType, BeliefValueType, BeliefMDPType>::setFMSchedValueList(
     std::vector<std::vector<std::unordered_map<uint64_t, ValueType>>> valueList) {
-    this->interactiveUnderApproximationExplorer->setFMSchedValueList(valueList);
+    interactiveUnderApproximationExplorer->setFMSchedValueList(valueList);
 }
 
 template<typename PomdpModelType, typename BeliefValueType, typename BeliefMDPType>
