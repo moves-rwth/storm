e6e6fdd8da0f39e0a65104adec7621da4dfe9788
Linus Heck <linus.heck@ru.nl>
2024-11-01 16:14:35 +0100

    iMC changes outside of storm-pars

--- src/storm/adapters/Z3ExpressionAdapter.cpp
+++ src/storm/adapters/Z3ExpressionAdapter.cpp
@@ -166,6 +166,9 @@ storm::expressions::Expression Z3ExpressionAdapter::translateExpression(z3::expr
                         return manager.rational(storm::utility::convertNumber<storm::RationalNumber>(std::string(Z3_get_numeral_string(expr.ctx(), expr))));
                     }
                 }
+            case Z3_OP_AGNUM:
+                return manager.rational(storm::utility::convertNumber<storm::RationalNumber>(
+                    std::string(Z3_get_numeral_string(expr.ctx(), Z3_get_algebraic_number_lower(expr.ctx(), expr, 16)))));
             case Z3_OP_UNINTERPRETED:
                 // Currently, we only support uninterpreted constant functions.
                 STORM_LOG_THROW(expr.is_const(), storm::exceptions::ExpressionEvaluationException,
