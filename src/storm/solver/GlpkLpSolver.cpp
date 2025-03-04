4b3fdee5eac141068c7a620c6d4c1883d4aac732
Sebastian Junges <sebastian.junges@gmail.com>
2025-03-04 17:45:26 +0100

    Update src/storm/solver/GlpkLpSolver.cpp
    Co-authored-by: Matthias Volk <volkm@users.noreply.github.com>

--- src/storm/solver/GlpkLpSolver.cpp
+++ src/storm/solver/GlpkLpSolver.cpp
@@ -475,7 +475,7 @@ bool GlpkLpSolver<ValueType, RawMode>::getBinaryValue(Variable const& variable) 
     return static_cast<bool>(value);
 #else
     throw storm::exceptions::NotImplementedException() << "This version of storm was compiled without support for GLPK. Yet, a method was called that "
-                                                          "requires this support. Please choose a version of support with GLPK support.";
+                                                          "requires this support. Please choose a version with GLPK support.";
 #endif
 }
 
