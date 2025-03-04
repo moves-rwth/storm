664a4e8edcd159ed854302029d87f1ddb7e10aa5
Sebastian Junges <sebastian.junges@gmail.com>
2025-03-04 17:45:55 +0100

    Update src/storm/solver/GlpkLpSolver.cpp
    Co-authored-by: Matthias Volk <volkm@users.noreply.github.com>

--- src/storm/solver/GlpkLpSolver.cpp
+++ src/storm/solver/GlpkLpSolver.cpp
@@ -342,7 +342,7 @@ void GlpkLpSolver<ValueType, RawMode>::optimize() const {
     this->currentModelHasBeenOptimized = true;
 #else
     throw storm::exceptions::NotImplementedException() << "This version of storm was compiled without support for GLPK. Yet, a method was called that "
-                                                          "requires this support. Please choose a version of support with GLPK support.";
+                                                          "requires this support. Please choose a version with GLPK support.";
 #endif
 }
 
