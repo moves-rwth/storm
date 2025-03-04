292f23ce673e46bf0fa3de2bf1cfd27e3a52ab19
Sebastian Junges <sebastian.junges@gmail.com>
2025-03-04 17:48:46 +0100

    Update src/storm/solver/GlpkLpSolver.cpp
    Co-authored-by: Matthias Volk <volkm@users.noreply.github.com>

--- src/storm/solver/GlpkLpSolver.cpp
+++ src/storm/solver/GlpkLpSolver.cpp
@@ -360,7 +360,7 @@ bool GlpkLpSolver<ValueType, RawMode>::isInfeasible() const {
     }
 #else
     throw storm::exceptions::NotImplementedException() << "This version of storm was compiled without support for GLPK. Yet, a method was called that "
-                                                          "requires this support. Please choose a version of support with GLPK support.";
+                                                          "requires this support. Please choose a version with GLPK support.";
 #endif
 }
 
