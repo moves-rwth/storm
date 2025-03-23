aa040f45aad74f98850aac625830476e54c01498
Sebastian Junges <sebastian.junges@ru.nl>
2025-03-23 17:06:32 +0100

    update

--- resources/3rdparty/include_spot.cmake
+++ resources/3rdparty/include_spot.cmake
@@ -1,6 +1,7 @@
 set(STORM_HAVE_SPOT OFF)
+set(STORM_SHIPPED_SPOT OFF)
 
-if(TRUE)
+if(TRUE) # For easier disabling.
     # try to find Spot on the system
     if (NOT "${SPOT_ROOT}" STREQUAL "")
         message(STATUS "Storm - searching for Spot in ${SPOT_ROOT}")
@@ -35,8 +36,14 @@ if(TRUE)
     endif()
 endif()
 
-set(STORM_SHIPPED_SPOT OFF)
-if(FALSE)
+if(NOT SPOT_FOUND AND STORM_USE_SPOT_SHIPPED)
+    # Spot does not set library IDs with an rpath but with an absolute path.
+    if (APPLE)
+        set(SPOT_RPATH_FIX_COMMAND "install_name_tool;-id;@rpath/libspot.dylib;${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libspot${DYNAMIC_EXT}")
+    else()
+        set(SPOT_RPATH_FIX_COMMAND "")
+    endif()
+
     # download and install shipped Spot as shared libraries.
     # Note that configuring static libraries requires various dependencies which was rather cumbersome.
     # As of '25/3, SJ did not get this to work.
@@ -49,15 +56,17 @@ if(FALSE)
         CONFIGURE_COMMAND ${STORM_3RDPARTY_BINARY_DIR}/spot_src/configure --prefix=${STORM_3RDPARTY_BINARY_DIR}/spot --disable-python #--enable-static --disable-shared
         BUILD_COMMAND make -j${STORM_RESOURCES_BUILD_JOBCOUNT}
         INSTALL_COMMAND make install -j${STORM_RESOURCES_BUILD_JOBCOUNT}
+            COMMAND ${SPOT_RPATH_FIX_COMMAND}
+           # COMMAND ${BDDX_RPATH_FIX_COMMAND}
         LOG_CONFIGURE ON
         LOG_BUILD ON
         LOG_INSTALL ON
-        BUILD_BYPRODUCTS ${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libspot${DYNAMIC_EXT};${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libbddx${DYNAMIC_EXT}
+        BUILD_BYPRODUCTS ${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libspot.0${DYNAMIC_EXT};${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libbddx.0${DYNAMIC_EXT}
     )
     add_dependencies(storm_resources spot)
 
     set(SPOT_INCLUDE_DIR "${STORM_3RDPARTY_BINARY_DIR}/spot/include/")
-    set(SPOT_LIBRARIES "${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libspot${DYNAMIC_EXT};${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libbddx${DYNAMIC_EXT}")
+    set(SPOT_LIBRARIES "${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libspot.0${DYNAMIC_EXT};${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libbddx.0${DYNAMIC_EXT}")
     set(SPOT_INSTALL_DIR ${STORM_RESOURCE_INCLUDE_INSTALL_DIR}/spot/)
     set(STORM_HAVE_SPOT ON)
     set(STORM_SHIPPED_SPOT ON)
@@ -67,20 +76,18 @@ if(FALSE)
     add_library(Storm::spot-bddx SHARED IMPORTED)
     set_target_properties(Storm::spot-bddx PROPERTIES
             INTERFACE_INCLUDE_DIRECTORIES "${STORM_3RDPARTY_BINARY_DIR}/spot/include/"
-            IMPORTED_LOCATION ${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libbddx${DYNAMIC_EXT})
+            IMPORTED_LOCATION ${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libbddx.0${DYNAMIC_EXT})
 
     add_library(Storm::spot SHARED IMPORTED)
     set_target_properties(Storm::spot PROPERTIES
             INTERFACE_INCLUDE_DIRECTORIES "${STORM_3RDPARTY_BINARY_DIR}/spot/include/"
-            IMPORTED_LOCATION ${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libspot${DYNAMIC_EXT}
+            IMPORTED_LOCATION ${STORM_3RDPARTY_BINARY_DIR}/spot/lib/libspot.0${DYNAMIC_EXT}
             INTERFACE_LINK_LIBRARIES Storm::spot-bddx
     )
 
     install(FILES ${SPOT_LIBRARIES} DESTINATION ${STORM_RESOURCE_LIBRARY_INSTALL_DIR})
     install(DIRECTORY ${SPOT_INCLUDE_DIR}/ DESTINATION ${SPOT_INSTALL_DIR}
             FILES_MATCHING PATTERN "*.h" PATTERN "*.hh" PATTERN ".git" EXCLUDE)
-
-
 
     list(APPEND STORM_DEP_IMP_TARGETS Storm::spot)
     add_dependencies(storm_resources Storm::spot  )
