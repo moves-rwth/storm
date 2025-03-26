8bb20dc4acd14c54fe80543586889a1ff0b46e8c
Sebastian Junges <sebastian.junges@ru.nl>
2025-03-26 10:58:42 +0100

    update

--- resources/cmake/find_modules/FindMATHSAT.cmake
+++ resources/cmake/find_modules/FindMATHSAT.cmake
@@ -4,18 +4,12 @@
 #  MATHSAT_INCLUDE_DIRS - The mathsat include directory
 #  MATHSAT_LIBRARIES - The libraries needed to use mathsat
 find_path(MATHSAT_INCLUDE_DIRS NAMES mathsat.h
-PATHS ENV PATH INCLUDE "${MATHSAT_ROOT}/include" "/usr/include/mathsat" "/usr/local/include/mathsat/"
+    PATHS ENV PATH INCLUDE "${MATHSAT_ROOT}/include" "/usr/include/mathsat" "/usr/local/include/mathsat/" "/opt/mathsat/include"
 )
-if(${OPERATING_SYSTEM} MATCHES "Linux")
 find_library(MATHSAT_LIBRARIES mathsat
-PATHS ENV PATH INCLUDE "${MATHSAT_ROOT}/lib"
+    PATHS ENV PATH INCLUDE "${MATHSAT_ROOT}/lib" "/usr/include/mathsat" "/usr/local/include/mathsat/" "/opt/mathsat/lib"
 )
-else()
-# on macOS, the .dylib file has some hard coded path (Version 5.5.4) and we therefore link statically
-find_library(MATHSAT_LIBRARIES NAMES libmathsat${STATIC_EXT} mathsat
-PATHS ENV PATH INCLUDE "${MATHSAT_ROOT}/lib"
-)
-endif()
+
 # handle the QUIETLY and REQUIRED arguments and set MATHSAT_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
