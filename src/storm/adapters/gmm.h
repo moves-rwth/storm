2b2633a8c04e056f99a5ec6c79e2981eb3cd575d
Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
2025-09-01 15:39:34 +0200

    Reduce number of disabled warnings

--- src/storm/adapters/gmm.h
+++ src/storm/adapters/gmm.h
@@ -5,13 +5,11 @@
 
 #if defined(__clang__)
 #pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-variable"
 #pragma clang diagnostic ignored "-Wunused-parameter"
-#pragma clang diagnostic ignored "-Wunknown-pragmas"
 #elif defined(__GNUC__)
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#pragma GCC diagnostic ignored "-Wmisleading-indentation"
+// #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+// #pragma GCC diagnostic ignored "-Wmisleading-indentation"
 #endif
 
 #include <gmm/gmm_kernel.h>
@@ -24,11 +22,5 @@
 #include <gmm/gmm_solver_bicgstab.h>
 #include <gmm/gmm_solver_gmres.h>
 #include <gmm/gmm_solver_qmr.h>
-
-#if defined(__clang__)
-#pragma clang diagnostic pop
-#elif defined(__GNUC__)
-#pragma GCC diagnostic pop
-#endif
 
 #endif
