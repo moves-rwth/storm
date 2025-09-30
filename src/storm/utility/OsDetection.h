19ea344a5acb26a388bae9763564d5efb5c3e1b3
Matthias Volk <m.volk@tue.nl>
2025-09-30 23:49:37 +0200

    Extend Sylvan workaround from APPLE_SILICON to all ARM processors

--- src/storm/utility/OsDetection.h
+++ src/storm/utility/OsDetection.h
@@ -5,11 +5,12 @@
 #elif defined TARGET_OS_MAC || defined __apple__ || defined __APPLE__
 #define MACOS
 #define _DARWIN_USE_64_BIT_INODE #This relates to stat / stat64.Unsure if still needed.
-#ifdef __arm64__
-#define APPLE_SILICON
-#endif
 #elif defined _WIN32 || defined _WIN64
 #error Windows detected, but not supported.
 #else
 #error Could not detect Operating System
+#endif
+
+#ifdef __aarch64__
+#define ARM
 #endif
