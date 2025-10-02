c0cf954913b9133f08ea7cb3c689ac42e104bd8e
Linus Heck <linus.heck@ru.nl>
2025-10-02 14:38:04 +0200

    Fix monotonicity import

--- src/storm-pars/transformer/RobustParameterLifter.h
+++ src/storm-pars/transformer/RobustParameterLifter.h
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <vector>
 
+#include "storm-pars/analysis/LocalMonotonicityResult.h"
 #include "storm-pars/analysis/Order.h"
 #include "storm-pars/storage/ParameterRegion.h"
 #include "storm-pars/transformer/TimeTravelling.h"
