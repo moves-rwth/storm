a59bd18b078bb3cd57089d9354d91724b7429463
Matthias Volk <m.volk@tue.nl>
2025-10-16 17:07:25 +0200

    Try to ignore unknown warning option

--- src/storm/adapters/ExprttkAdapter.h
+++ src/storm/adapters/ExprttkAdapter.h
@@ -2,14 +2,12 @@
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wunknown-warning-option"
 #pragma clang diagnostic ignored "-Wunnecessary-virtual-specifier"
-
-#pragma GCC diagnostic push
 
 // exprtk should be case sensitive in our case.
 #define exprtk_disable_caseinsensitivity
 
 #include "exprtk.hpp"
 
-#pragma GCC diagnostic pop
 #pragma clang diagnostic pop
