afe98968249207b9b60c63793cd9a1d480a4f314
Matthias Volk <m.volk@tue.nl>
2025-12-16 11:30:41 +0100

    Removed define STORM_CARL_SUPPORTS_FWD_DECL

--- src/storm/adapters/RationalFunctionAdapter.h
+++ src/storm/adapters/RationalFunctionAdapter.h
@@ -8,11 +8,7 @@ RationalFunctionVariable createRFVariable(std::string const& name);
 
 }  // namespace storm
 
-#ifdef STORM_CARL_SUPPORTS_FWD_DECL
-// The version of carl that supported forward declarations
-// includes a bugfix also required for instantiating these templates once
 extern template class carl::MultivariatePolynomial<storm::RationalFunctionCoefficient>;
 extern template class carl::FactorizedPolynomial<storm::RawPolynomial>;
 extern template class carl::Cache<carl::PolynomialFactorizationPair<storm::RawPolynomial>>;
 extern template class carl::RationalFunction<storm::Polynomial, true>;
-#endif
--- src/storm/adapters/RationalFunctionForward.h
+++ src/storm/adapters/RationalFunctionForward.h
@@ -2,21 +2,8 @@
 #include "storm-config.h"
 
 #include "storm/adapters/RationalNumberForward.h"
-#ifdef STORM_CARL_SUPPORTS_FWD_DECL
+
 #include <carl/core/MultivariatePolynomialForward.h>
-#else
-// This must be included first
-// (for old version. Once STORM_CARL_SUPPORTS_FWD_DECL is true, this is no longer relevant).
-#include <carl/numbers/numbers.h>
-// These must be included later...
-#include <carl/core/FactorizedPolynomial.h>
-#include <carl/core/MultivariatePolynomial.h>
-#include <carl/core/RationalFunction.h>
-#include <carl/core/Relation.h>
-#include <carl/core/UnivariatePolynomial.h>
-#include <carl/core/VariablePool.h>
-#include "storm/adapters/RationalNumberAdapter.h"
-#endif
 
 namespace carl {
 
