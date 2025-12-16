20f5c1ca377be11fb7d698e8aa845d4249df044f
Matthias Volk <m.volk@tue.nl>
2025-12-16 18:17:24 +0100

    Merged RationalFunctionAdapter_Private into RationalFunctionAdapter

--- src/storm/adapters/RationalFunctionAdapter_Private.cpp
+++
@@ -1,7 +0,0 @@
-#include "storm/adapters/RationalFunctionAdapter_Private.h"
-
-// See RationalFunctionAdapter.h
-template class carl::MultivariatePolynomial<storm::RationalFunctionCoefficient>;
-template class carl::FactorizedPolynomial<storm::RawPolynomial>;
-template class carl::Cache<carl::PolynomialFactorizationPair<storm::RawPolynomial>>;
-template class carl::RationalFunction<storm::Polynomial, true>;
