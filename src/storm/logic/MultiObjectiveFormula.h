70b182f790c18a40573ed6d6050255881fd2d813
Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
2026-01-04 06:20:24 +0100

    Multi-objective property handling (#805)
    - Adds a `type` to MultiObjectiveFormulas (tradeoff or lexicographic).
    - Allows specifying multi-objective (tradeoff and lexicographic) properties within a JANI model.
    - Introduces the `multilex(..)` syntax for Prism-style properties.
    - Supports conversion from Prism-style property specifications to JANI.
    - Deprecates the `--[multiobjective:]lex` setting.
    - Fixed Jani export of total and cumulative reward formulas

--- src/storm/logic/MultiObjectiveFormula.h
+++ src/storm/logic/MultiObjectiveFormula.h
@@ -7,16 +7,21 @@ namespace storm {
 namespace logic {
 class MultiObjectiveFormula : public StateFormula {
    public:
-    MultiObjectiveFormula(std::vector<std::shared_ptr<Formula const>> const& subformulas);
+    enum class Type { Tradeoff, Lexicographic };
+
+    MultiObjectiveFormula(std::vector<std::shared_ptr<Formula const>> const& subformulas, Type type);
 
     virtual ~MultiObjectiveFormula();
 
     virtual bool isMultiObjectiveFormula() const override;
 
+    bool isTradeoff() const;
+    bool isLexicographic() const;
+    Type getType() const;
     virtual bool hasQualitativeResult() const override;   // Result is true or false
     virtual bool hasQuantitativeResult() const override;  // Result is numerical or a pareto curve
-    virtual bool hasNumericalResult() const;              // Result is numerical
-    virtual bool hasParetoCurveResult() const;            // Result is a pareto curve
+    virtual bool hasNumericalResult() const;              // Result is numerical (1-dimensional)
+    virtual bool hasMultiDimensionalResult() const;       // Result is multi-dimensional (pareto curve or point)
 
     Formula const& getSubformula(uint_fast64_t index) const;
     uint_fast64_t getNumberOfSubformulas() const;
@@ -26,11 +31,13 @@ class MultiObjectiveFormula : public StateFormula {
     virtual void gatherAtomicExpressionFormulas(std::vector<std::shared_ptr<AtomicExpressionFormula const>>& atomicExpressionFormulas) const override;
     virtual void gatherAtomicLabelFormulas(std::vector<std::shared_ptr<AtomicLabelFormula const>>& atomicLabelFormulas) const override;
     virtual void gatherReferencedRewardModels(std::set<std::string>& referencedRewardModels) const override;
+    virtual void gatherUsedVariables(std::set<storm::expressions::Variable>& usedVariables) const override;
 
     virtual std::ostream& writeToStream(std::ostream& out, bool allowParentheses = false) const override;
 
    private:
     std::vector<std::shared_ptr<Formula const>> subformulas;
+    Type type;
 };
 }  // namespace logic
 }  // namespace storm
--- src/storm/storage/jani/ModelFeatures.h
+++ src/storm/storage/jani/ModelFeatures.h
@@ -6,7 +6,7 @@
 namespace storm {
 namespace jani {
 
-enum class ModelFeature { Arrays, DerivedOperators, Functions, StateExitRewards, TrigonometricFunctions };
+enum class ModelFeature { Arrays, DerivedOperators, Functions, StateExitRewards, MultiObjectiveProperties, TrigonometricFunctions };
 
 std::string toString(ModelFeature const& modelFeature);
 
@@ -18,6 +18,7 @@ class ModelFeatures {
     bool hasFunctions() const;
     bool hasDerivedOperators() const;
     bool hasStateExitRewards() const;
+    bool hasMultiObjectiveProperties() const;
     bool hasTrigonometricFunctions() const;
 
     // Returns true, if no model feature is enabled.
