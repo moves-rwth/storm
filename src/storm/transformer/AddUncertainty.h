db2357b41c0fdcadc7a5d8bcebb1597cbda59864
Luko van der Maas <luko.vandermaas@ru.nl>
2026-03-19 13:58:13 +0100

    cache test

--- src/storm/transformer/AddUncertainty.h
+++ src/storm/transformer/AddUncertainty.h
@@ -16,7 +16,7 @@ template<typename ValueType>
 class AddUncertainty {
    public:
     AddUncertainty(std::shared_ptr<storm::models::sparse::Model<ValueType>> const& originalModel);
-    std::shared_ptr<storm::models::sparse::Model<Interval>> transform(double additiveUncertainty, double minimalValue = 0.0001);
+    std::shared_ptr<storm::models::sparse::Model<Interval>> transform(double additiveUncertainty, double minimalValue = 0.001);
 
    private:
     storm::Interval addUncertainty(ValueType const& vt, double additiveUncertainty, double minimalValue);
